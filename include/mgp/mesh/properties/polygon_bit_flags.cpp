c93d25cd61595c33e9d8cf70d9fb78a6626bd592
alemuntoni <muntoni.alessandro@gmail.com>
2021-11-05 19:52:52 +0100

    set bits for PolygonBitFlags

--- include/mgp/mesh/properties/polygon_bit_flags.cpp
+++ include/mgp/mesh/properties/polygon_bit_flags.cpp
@@ -7,4 +7,44 @@
 
 namespace mgp::prop {
 
+/**
+ * @brief  PolygonBitFlags::userBitFlag returns the value of the bit given in input. The bit is
+ * checked to be greater or equal than the first user bit available in this class (24) and less
+ * than 32.
+ *
+ * @param bit: the position of the bit that will be returned.
+ * @return true if the required bit is enabled, false otherwise.
+ */
+inline bool PolygonBitFlags::userBitFlag(unsigned int bit) const
+{
+	// using FIRST_USER_BIT of this class
+	return BitFlags::userBitFlag(bit, FIRST_USER_BIT);
+}
+
+/**
+ * @brief PolygonBitFlags::setUserBit sets to true the value of the bit given in input. The bit is
+ * checked to be greater or equal than the first user bit available in this class (24) and less
+ * than 32.
+ *
+ * @param bit: the position of the bit that will be set.
+ */
+inline void PolygonBitFlags::setUserBit(unsigned int bit)
+{
+	// using FIRST_USER_BIT of this class
+	BitFlags::setUserBit(bit, FIRST_USER_BIT);
+}
+
+/**
+ * @brief PolygonBitFlags::clearUserBit sets to false the value of the bit given in input. The bit
+ * is checked to be greater or equal than the first user bit available in this class (24) and less
+ * than 32.
+ *
+ * @param bit: the position of the bit that will be reset.
+ */
+inline void PolygonBitFlags::clearUserBit(unsigned int bit)
+{
+	// using FIRST_USER_BIT of this class
+	BitFlags::clearUserBit(bit, FIRST_USER_BIT);
+}
+
 }
--- include/mgp/mesh/properties/polygon_bit_flags.h
+++ include/mgp/mesh/properties/polygon_bit_flags.h
@@ -13,9 +13,43 @@ namespace mgp::prop {
 class PolygonBitFlags : public BitFlags
 {
 public:
+	// member fuction that hide base members (to use the FIRST_USER_BIT value set here)
+	bool userBitFlag(unsigned int bit) const;
+	void setUserBit(unsigned int bit);
+	void clearUserBit(unsigned int bit);
 
 protected:
-	int flags2 = 0;
+	// hide base class constant, 23 is the number of bits used by this class
+	static const unsigned int FIRST_USER_BIT = BitFlags::FIRST_USER_BIT + 23;
+
+	// values of the flags, used for flagValue, setFlag and clearFlag member functions
+	enum {
+		// BORDER0 is BORDER, inherited from superclass
+		BORDER1  = 1 << (BitFlags::FIRST_USER_BIT),
+		BORDER2  = 1 << (BitFlags::FIRST_USER_BIT + 1),
+		BORDER3  = 1 << (BitFlags::FIRST_USER_BIT + 2),
+		BORDER4  = 1 << (BitFlags::FIRST_USER_BIT + 3),
+		BORDER5  = 1 << (BitFlags::FIRST_USER_BIT + 4),
+		BORDER6  = 1 << (BitFlags::FIRST_USER_BIT + 5),
+		BORDER7  = 1 << (BitFlags::FIRST_USER_BIT + 6),
+		BORDER8  = 1 << (BitFlags::FIRST_USER_BIT + 7),
+		BORDER9  = 1 << (BitFlags::FIRST_USER_BIT + 8),
+		BORDER10 = 1 << (BitFlags::FIRST_USER_BIT + 9),
+		BORDER11 = 1 << (BitFlags::FIRST_USER_BIT + 10), // 12 bits for border
+		// Edge selection
+		EDGESEL0  = 1 << (BitFlags::FIRST_USER_BIT + 11),
+		EDGESEL1  = 1 << (BitFlags::FIRST_USER_BIT + 12),
+		EDGESEL2  = 1 << (BitFlags::FIRST_USER_BIT + 13),
+		EDGESEL3  = 1 << (BitFlags::FIRST_USER_BIT + 14),
+		EDGESEL4  = 1 << (BitFlags::FIRST_USER_BIT + 15),
+		EDGESEL5  = 1 << (BitFlags::FIRST_USER_BIT + 16),
+		EDGESEL6  = 1 << (BitFlags::FIRST_USER_BIT + 17),
+		EDGESEL7  = 1 << (BitFlags::FIRST_USER_BIT + 18),
+		EDGESEL8  = 1 << (BitFlags::FIRST_USER_BIT + 19),
+		EDGESEL9  = 1 << (BitFlags::FIRST_USER_BIT + 20),
+		EDGESEL10 = 1 << (BitFlags::FIRST_USER_BIT + 21),
+		EDGESEL11 = 1 << (BitFlags::FIRST_USER_BIT + 22) // 12 bits for selection
+	};
 
 private:
 	// will use these members as isOnBorder0, setOnBorder0 and clearOnBorder0
