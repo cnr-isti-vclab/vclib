ffc51b6f4aaf75426f793aaf4b0acd41ed1e6c8f
alemuntoni <muntoni.alessandro@gmail.com>
2021-11-18 10:17:25 +0100

    add ElementContainer

---
+++ include/mgp/mesh/container/element_container.h
@@ -0,0 +1,27 @@
+/**
+ * This file is part of mgplib: https://github.com/alemuntoni/mgplib
+ * This Source Code Form is subject to the terms of the GNU GPL 3.0
+ */
+
+#ifndef MGP_MESH_CONTAINER_ELEMENT_CONTAINER_H
+#define MGP_MESH_CONTAINER_ELEMENT_CONTAINER_H
+
+#include <vector>
+#include "../components_vector/components_vector.h"
+
+namespace mgp::mesh {
+
+template <typename T>
+class ElementContainer
+{
+public:
+	ElementContainer();
+
+protected:
+	std::vector<T> vec;
+	internal::ComponentsVector<T> optionalVec;
+};
+
+}
+
+#endif // MGP_MESH_CONTAINER_ELEMENT_CONTAINER_H
