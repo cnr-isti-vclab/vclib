1eab467d7cc3e96f343fc916e83b1aeb919167f3
alemuntoni <muntoni.alessandro@gmail.com>
2022-12-27 14:57:01 +0100

    update test viewer

--- test/ext/qglviewer/viewer/main.cpp
+++ test/ext/qglviewer/viewer/main.cpp
@@ -27,32 +27,46 @@
 #include <vclib/algorithms/update/normal.h>
 #include <vclib/algorithms/update/color.h>
 #include <vclib/tri_mesh.h>
+#include <vclib/poly_mesh.h>
+#include <vclib/io/load_obj.h>
 #include <vclib/io/load_ply.h>
 
 #include <vclib/ext/opengl2/drawable_mesh.h>
-#include <vclib/ext/qglviewer/viewer.h>
+#include <vclib/ext/qglviewer/viewer_main_window.h>
+#include <vclib/ext/qt/gui/mesh_render_settings_frame.h>
 
 int main(int argc, char **argv) {
 	// Read command lines arguments.
 	QApplication application(argc, argv);
 
 	// Instantiate the viewer.
-	vcl::Viewer viewer;
-
-	viewer.setWindowTitle("simpleViewer");
+	vcl::ViewerMainWindow viewer;
 
 	vcl::io::FileMeshInfo loadedInfo;
-	vcl::TriMesh m = vcl::io::loadPly<vcl::TriMesh>(VCL_TEST_MODELS_PATH "/brain.ply", loadedInfo);
 
+	vcl::PolyMesh tm = vcl::io::loadPly<vcl::PolyMesh>(VCL_TEST_MODELS_PATH "/VertTextureDouble.ply", loadedInfo);
+	vcl::updatePerFaceNormals(tm);
+	vcl::updatePerVertexNormals(tm);
+	tm.enablePerFaceColor();
+	vcl::setPerVertexColor(tm, vcl::Color::DarkMagenta);
+	vcl::setPerFaceColor(tm, vcl::Color::LightGreen);
+	vcl::setMeshColor(tm, vcl::Color::Yellow);
+
+	vcl::TriMesh m = vcl::io::loadObj<vcl::TriMesh>(VCL_TEST_MODELS_PATH "/bimba.obj");
 	vcl::updatePerFaceNormals(m);
 	vcl::updatePerVertexNormals(m);
-	vcl::setPerVertexColor(m, vcl::Color::White);
+	vcl::setPerVertexColor(m, vcl::Color::DarkGreen);
 
-	vcl::MeshRenderSettings mrs;
-	vcl::DrawableMesh<vcl::TriMesh> dtm(m, mrs);
+	vcl::DrawableMesh<vcl::PolyMesh> dtm(tm);
+	vcl::DrawableMesh<vcl::TriMesh> dm(m);
 
-	viewer.pushDrawableObject(dtm);
-	viewer.fitScene();
+	std::shared_ptr<vcl::DrawableObjectVector> vector = std::make_shared<vcl::DrawableObjectVector>();
+	vector->pushBack(dtm);
+	vector->pushBack(dm);
+
+	viewer.setDrawableObjectVector(vector);
+
+	viewer.setWindowTitle("simpleViewer");
 
 	// Make the viewer window visible on screen.
 	viewer.show();
