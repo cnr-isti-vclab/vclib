cf877cfb8fa174c93c7b6abfee3af76ecf743f79
alemuntoni <muntoni.alessandro@gmail.com>
2023-02-07 15:06:47 +0100

    Add Vertical Components Vector Tuple class

---
+++ include/vclib/mesh/containers/vertical_components_vector_tuple.cpp
@@ -0,0 +1,127 @@
+/*****************************************************************************
+ * VCLib                                                                     *
+ * Visual Computing Library                                                  *
+ *                                                                           *
+ * Copyright(C) 2021-2023                                                    *
+ * Alessandro Muntoni                                                        *
+ * Visual Computing Lab                                                      *
+ * ISTI - Italian National Research Council                                  *
+ *                                                                           *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This program is free software; you can redistribute it and/or modify      *
+ * it under the terms of the GNU General Public License as published by      *
+ * the Free Software Foundation; either version 3 of the License, or         *
+ * (at your option) any later version.                                       *
+ *                                                                           *
+ * This program is distributed in the hope that it will be useful,           *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              *
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
+ * for more details.                                                         *
+ ****************************************************************************/
+
+#include "vertical_components_vector_tuple.h"
+
+namespace vcl::mesh {
+
+template<typename ...Comp>
+ComponentsVectorTuple<Comp...>::ComponentsVectorTuple()
+{
+	// by default, all the components are enabled
+	vecEnabled.fill(true);
+}
+
+template<typename ...Comp>
+constexpr uint ComponentsVectorTuple<Comp...>::componentsNumber()
+{
+	return std::tuple_size_v<std::tuple<std::vector<Comp>...>>;
+}
+
+template<typename ...Comp>
+template<typename C>
+constexpr uint ComponentsVectorTuple<Comp...>::indexOfType()
+{
+	return vcl::indexInTypePack<C, Comp...>();
+}
+
+template<typename ...Comp>
+template<typename C>
+constexpr std::vector<typename C::ValueType>& ComponentsVectorTuple<Comp...>::vector()
+{
+	constexpr uint ind = indexOfType<C>();
+	return std::get<ind>(tuple);
+}
+
+template<typename ...Comp>
+template<typename C>
+constexpr const std::vector<typename C::ValueType>& ComponentsVectorTuple<Comp...>::vector() const
+{
+	constexpr uint ind = indexOfType<C>();
+	return std::get<ind>(tuple);
+}
+
+template<typename ...Comp>
+std::size_t ComponentsVectorTuple<Comp...>::size() const
+{
+	return siz;
+}
+
+template<typename ...Comp>
+void ComponentsVectorTuple<Comp...>::resize(std::size_t size)
+{
+	vectorResize<componentsNumber()-1>(size);
+	siz = size;
+}
+
+template<typename ...Comp>
+void ComponentsVectorTuple<Comp...>::clear()
+{
+	auto function =
+		[](auto&... args)
+	{
+		((args.clear()), ...);
+	};
+
+	std::apply(function, tuple);
+	siz = 0;
+}
+
+template<typename ...Comp>
+template<typename C>
+bool ComponentsVectorTuple<Comp...>::isComponentEnabled() const
+{
+	constexpr uint ind = indexOfType<C>();
+	return vecEnabled[ind];
+}
+
+template<typename ...Comp>
+template<typename C>
+void ComponentsVectorTuple<Comp...>::enableComponent()
+{
+	constexpr uint ind = indexOfType<C>();
+	vecEnabled[ind] = true;
+	vector<C>().resize(siz);
+}
+
+template<typename ...Comp>
+template<typename C>
+void ComponentsVectorTuple<Comp...>::disableComponent()
+{
+	constexpr uint ind = indexOfType<C>();
+	vecEnabled[ind] = false;
+	vector<C>().clear();
+}
+
+template<typename ...Comp>
+template<std::size_t N>
+void ComponentsVectorTuple<Comp...>::vectorResize(std::size_t size)
+{
+	if (vecEnabled[N]) {
+		std::get<N>(tuple).resize(size);
+	}
+	if constexpr (N != 0)
+		vectorResize<N-1>(size);
+}
+
+} // namespace vcl::mesh
---
+++ include/vclib/mesh/containers/vertical_components_vector_tuple.h
@@ -0,0 +1,87 @@
+/*****************************************************************************
+ * VCLib                                                                     *
+ * Visual Computing Library                                                  *
+ *                                                                           *
+ * Copyright(C) 2021-2023                                                    *
+ * Alessandro Muntoni                                                        *
+ * Visual Computing Lab                                                      *
+ * ISTI - Italian National Research Council                                  *
+ *                                                                           *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This program is free software; you can redistribute it and/or modify      *
+ * it under the terms of the GNU General Public License as published by      *
+ * the Free Software Foundation; either version 3 of the License, or         *
+ * (at your option) any later version.                                       *
+ *                                                                           *
+ * This program is distributed in the hope that it will be useful,           *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              *
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
+ * for more details.                                                         *
+ ****************************************************************************/
+
+#ifndef VCL_MESH_CONTAINER_VERTICAL_COMPONENTS_VECTOR_TUPLE_H
+#define VCL_MESH_CONTAINER_VERTICAL_COMPONENTS_VECTOR_TUPLE_H
+
+#include <array>
+#include <vector>
+#include <tuple>
+
+#include <vclib/misc/types.h>
+#include <vclib/misc/tuple.h>
+
+namespace vcl::mesh {
+
+template<typename ...Comp>
+class ComponentsVectorTuple
+{
+	using ComponentTypes = std::tuple<Comp...>;
+public:
+	ComponentsVectorTuple();
+
+	static constexpr uint componentsNumber();
+
+	template<typename C>
+	static constexpr uint indexOfType();
+
+	template<typename C>
+	constexpr std::vector<typename C::ValueType>& vector();
+
+	template<typename C>
+	constexpr const std::vector<typename C::ValueType>& vector() const;
+
+	std::size_t size() const;
+
+	void resize(std::size_t size);
+	void clear();
+
+	template<typename C>
+	bool isComponentEnabled() const;
+
+	template<typename C>
+	void enableComponent();
+
+	template<typename C>
+	void disableComponent();
+
+private:
+	std::tuple<std::vector<typename Comp::DataValueType>...> tuple;
+
+	std::array<bool, componentsNumber()> vecEnabled;
+	std::size_t siz = 0;
+
+	template<std::size_t N>
+	void vectorResize(std::size_t size);
+};
+
+template<typename ...Comp>
+class ComponentsVectorTuple<std::tuple<Comp...>> : public ComponentsVectorTuple<Comp...>
+{
+};
+
+} // namespace vcl::mesh
+
+#include "vertical_components_vector_tuple.cpp"
+
+#endif // VCL_MESH_CONTAINER_VERTICAL_COMPONENTS_VECTOR_TUPLE_H
