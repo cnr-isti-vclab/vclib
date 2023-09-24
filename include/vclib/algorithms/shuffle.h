85ec75c105e9281115a1b2f0b3d502882658303b
alemuntoni <muntoni.alessandro@gmail.com>
2023-09-24 15:50:36 +0200

    delete algorithms/shuffle.cpp

--- include/vclib/algorithms/shuffle.h
+++ include/vclib/algorithms/shuffle.h
@@ -27,8 +27,13 @@
 #include <random>
 
 #include <vclib/mesh/requirements.h>
+#include <vclib/misc/timer.h>
 
 namespace vcl {
+
+/****************
+ * Declarations *
+ ****************/
 
 template<MeshConcept MeshType>
 std::vector<typename MeshType::VertexType*>
@@ -53,8 +58,139 @@ fillAndShuffleFacePointerVector(const MeshType& m, bool deterministic = false);
 template<FaceMeshConcept MeshType>
 std::vector<uint> fillAndShuffleFaceIndexVector(const MeshType& m, bool deterministic = false);
 
-} // namespace vcl
+/***************
+ * Definitions *
+ ***************/
 
-#include "shuffle.cpp"
+namespace internal {
+
+/*
+ * Generic implementation of fillAndShuffleVertexPointerVector, to avoid code duplication.
+ *
+ * Templates M and V can be both const and non-const MeshType and VertexType
+ */
+template<typename M, typename V>
+std::vector<V*> genericFASVPV(M m, bool deterministic)
+{
+	std::vector<V*> vec;
+	vec.reserve(m.vertexNumber());
+
+	for (V& v : m.vertices()) {
+		vec.push_back(&v);
+	}
+
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	if (deterministic)
+		gen = std::mt19937(0);
+	std::shuffle(vec.begin(),vec.end(), gen);
+	return vec;
+}
+
+/*
+ * Generic implementation of fillAndShuffleFacePointerVector, to avoid code duplication.
+ *
+ * Templates M and F can be both const and non-const MeshType and FaceType
+ */
+template<typename M, typename F>
+std::vector<F*> genericFASFPV(M m, bool deterministic)
+{
+	std::vector<F*> vec;
+	vec.reserve(m.faceNumber());
+
+	for (F& f : m.faces()) {
+		vec.push_back(&f);
+	}
+
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	if (deterministic)
+		gen = std::mt19937(0);
+	std::shuffle(vec.begin(),vec.end(), gen);
+	return vec;
+}
+
+} // namespace internal
+
+template<MeshConcept MeshType>
+std::vector<typename MeshType::VertexType*>fillAndShuffleVertexPointerVector(
+	MeshType& m,
+	bool deterministic)
+{
+	using VertexType = MeshType::VertexType;
+
+	return internal::genericFASVPV<MeshType&, VertexType>(m, deterministic);
+}
+
+template<MeshConcept MeshType>
+std::vector<const typename MeshType::VertexType*> fillAndShuffleVertexPointerVector(
+	const MeshType& m,
+	bool deterministic)
+{
+	using VertexType = MeshType::VertexType;
+	return internal::genericFASVPV<const MeshType&, const VertexType>(m, deterministic);
+}
+
+template<MeshConcept MeshType>
+std::vector<uint>
+fillAndShuffleVertexIndexVector(const MeshType& m, bool deterministic)
+{
+	using VertexType = MeshType::VertexType;
+
+	std::vector<uint> vec;
+	vec.reserve(m.vertexNumber());
+
+	for (const VertexType& v : m.vertices()) {
+		vec.push_back(m.index(v));
+	}
+
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	if (deterministic)
+		gen = std::mt19937(0);
+	std::shuffle(vec.begin(),vec.end(), gen);
+	return vec;
+}
+
+template<FaceMeshConcept MeshType>
+std::vector<typename MeshType::FaceType*>
+fillAndShuffleFacePointerVector(MeshType& m, bool deterministic)
+{
+	using FaceType = MeshType::FaceType;
+
+	return internal::genericFASFPV<MeshType&, FaceType>(m, deterministic);
+}
+
+template<FaceMeshConcept MeshType>
+std::vector<const typename MeshType::FaceType*>
+fillAndShuffleFacePointerVector(const MeshType& m, bool deterministic)
+{
+	using FaceType = MeshType::FaceType;
+
+	return internal::genericFASFPV<const MeshType&, const FaceType>(m, deterministic);
+}
+
+template<FaceMeshConcept MeshType>
+std::vector<uint>
+fillAndShuffleFaceIndexVector(const MeshType& m, bool deterministic)
+{
+	using FaceType = MeshType::FaceType;
+
+	std::vector<uint> vec;
+	vec.reserve(m.faceNumber());
+
+	for (const FaceType& f : m.faces()) {
+		vec.push_back(m.index(f));
+	}
+
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	if (deterministic)
+		gen = std::mt19937(0);
+	std::shuffle(vec.begin(),vec.end(), gen);
+	return vec;
+}
+
+} // namespace vcl
 
 #endif // VCL_ALGORITHMS_SHUFFLE_H
