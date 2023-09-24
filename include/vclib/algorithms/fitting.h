e6d196a8cab790fd94e7a88fe2f30cccc30181ad
alemuntoni <muntoni.alessandro@gmail.com>
2023-09-24 15:42:46 +0200

    delete algorithms/fitting.cpp

--- include/vclib/algorithms/fitting.h
+++ include/vclib/algorithms/fitting.h
@@ -26,7 +26,13 @@
 
 #include <vclib/space/plane.h>
 
+#include "stat.h"
+
 namespace vcl {
+
+/****************
+ * Declarations *
+ ****************/
 
 template<typename Scalar>
 Plane<Scalar> fitPlaneToPointCloud(const std::vector<Point3<Scalar>>& pointVec);
@@ -34,8 +40,62 @@ Plane<Scalar> fitPlaneToPointCloud(const std::vector<Point3<Scalar>>& pointVec);
 template<typename Scalar>
 Plane<Scalar> fitPlaneToWeightedPointCloud(const std::vector<Point3<Scalar>>& pointVec);
 
-} // namespace vcl
+/***************
+ * Definitions *
+ ***************/
 
-#include "fitting.cpp"
+/**
+ * @brief Compute the plane best fitting a set of points
+ * The algorithm used is the classical Covariance matrix eigenvector approach.
+ */
+template<typename Scalar>
+Plane<Scalar> fitPlaneToPointCloud(const std::vector<Point3<Scalar>>& pointVec)
+{
+	Matrix33<Scalar> covMat = covarianceMatrixOfPointCloud(pointVec);
+	Point3<Scalar>   b      = polygonBarycenter(pointVec.begin(), pointVec.end());
+
+	Eigen::SelfAdjointEigenSolver<Eigen::Matrix<Scalar, 3, 3>> eig(covMat);
+
+	Eigen::Matrix<Scalar, 3, 1> eval = eig.eigenvalues();
+	Eigen::Matrix<Scalar, 3, 3> evec = eig.eigenvectors();
+
+	eval = eval.cwiseAbs();
+	int minInd;
+	eval.minCoeff(&minInd);
+	Point3<Scalar> d;
+	d[0] = evec(0, minInd);
+	d[1] = evec(1, minInd);
+	d[2] = evec(2, minInd);
+
+	return Plane<Scalar>(b, d);
+}
+
+/**
+ * @brief Compute the plane best fitting a wighted set of points
+ * The algorithm used is the wighted Covariance matrix eigenvector approach.
+ */
+template<typename Scalar>
+Plane<Scalar> fitPlaneToWeightedPointCloud(const std::vector<Point3<Scalar>>& pointVec)
+{
+	Matrix33<Scalar> covMat = weightedCovarianceMatrixOfPointCloud(pointVec);
+	Point3<Scalar>   b      = polygonWeighedBarycenter(pointVec);
+
+	Eigen::SelfAdjointEigenSolver<Eigen::Matrix<Scalar, 3, 3>> eig(covMat);
+
+	Eigen::Matrix<Scalar, 3, 1> eval = eig.eigenvalues();
+	Eigen::Matrix<Scalar, 3, 3> evec = eig.eigenvectors();
+
+	eval = eval.cwiseAbs();
+	int minInd;
+	eval.minCoeff(&minInd);
+	Point3<Scalar> d;
+	d[0] = evec(0, minInd);
+	d[1] = evec(1, minInd);
+	d[2] = evec(2, minInd);
+
+	return Plane<Scalar>(b, d);
+}
+
+} // namespace vcl
 
 #endif // VCL_ALGORITHMS_FITTING_H
