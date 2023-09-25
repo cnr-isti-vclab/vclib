df4bcba50b00db46929e5447e3cf6ac4f2e704f0
alemuntoni <muntoni.alessandro@gmail.com>
2023-09-25 17:49:18 +0200

    delete algorithms/distance/misc.cpp

--- include/vclib/algorithms/distance/misc.h
+++ include/vclib/algorithms/distance/misc.h
@@ -29,6 +29,10 @@
 
 namespace vcl {
 
+/****************
+ * Declarations *
+ ****************/
+
 template<Point3Concept PointType, PlaneConcept PlaneType>
 auto pointPlaneDistance(const PointType& p, const PlaneType& plane, bool signedDist = false);
 
@@ -40,8 +44,52 @@ template<PointConcept PointType, SegmentConcept SegmentType>
 auto pointSegmentDistance(const PointType& p, const SegmentType& s)
 	requires (PointType::DIM == SegmentType::DIM);
 
-} // namespace vcl
+/***************
+ * Definitions *
+ ***************/
 
-#include "misc.cpp"
+template<Point3Concept PointType, PlaneConcept PlaneType>
+auto pointPlaneDistance(const PointType& p, const PlaneType& plane, bool signedDist)
+{
+	auto dist = plane.direction().dot(p) - plane.offset();
+	if (!signedDist)
+		dist = std::abs(dist);
+	return dist;
+}
+
+template<PointConcept PointType, SegmentConcept SegmentType>
+auto pointSegmentDistance(const PointType& p, const SegmentType& s, PointType& closest)
+	requires (PointType::DIM == SegmentType::DIM)
+{
+	using ScalarType = PointType::ScalarType;
+
+	ScalarType dist;
+
+	PointType dir = s.direction();
+	ScalarType esn = dir.squaredNorm();
+
+	if (esn < std::numeric_limits<ScalarType>::min()) {
+		closest = s.midPoint();
+	}
+	else {
+		ScalarType t = ((p-s.p0())*dir)/esn;
+		if (t < 0) t = 0;
+		else if (t > 1) t = 1;
+
+		closest = s.p0() * (1-t) + s.p1() * t;
+	}
+	dist = p.dist(closest);
+	return dist;
+}
+
+template<PointConcept PointType, SegmentConcept SegmentType>
+auto pointSegmentDistance(const PointType& p, const SegmentType& s)
+	requires (PointType::DIM == SegmentType::DIM)
+{
+	PointType closest;
+	return pointSegmentDistance(p, s, closest);
+}
+
+} // namespace vcl
 
 #endif // VCL_ALGORITHMS_DISTANCE_MISC_H
