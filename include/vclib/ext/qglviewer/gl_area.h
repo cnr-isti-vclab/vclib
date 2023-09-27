afbe3e18e7ad4fa4c7891ab1fae9a6688473d0f1
alemuntoni <muntoni.alessandro@gmail.com>
2023-09-27 17:45:07 +0200

    delete all ext cpp files

--- include/vclib/ext/qglviewer/gl_area.h
+++ include/vclib/ext/qglviewer/gl_area.h
@@ -28,10 +28,15 @@
 
 #include <QGLViewer/qglviewer.h>
 
+#include <vclib/math/min_max.h>
 #include <vclib/render/drawable_object_vector.h>
 #include <vclib/space/box.h>
 
 namespace vcl {
+
+/******************************************************************************
+ *                                Declarations                                *
+ ******************************************************************************/
 
 class GLArea : public QGLViewer
 {
@@ -58,8 +63,94 @@ private:
 	vcl::Box3d fullBB() const;
 };
 
-} // namespace vcl
+/******************************************************************************
+ *                                Definitions                                 *
+ ******************************************************************************/
 
-#include "gl_area.cpp"
+inline GLArea::GLArea(QWidget* parent) :
+		QGLViewer(parent)
+{
+	drawList = std::make_shared<DrawableObjectVector>();
+}
+
+inline GLArea::GLArea(std::shared_ptr<DrawableObjectVector> v, QWidget* parent) :
+		QGLViewer(parent), drawList(v)
+{
+}
+
+inline void GLArea::init()
+{
+	for (DrawableObject* d : *drawList) {
+		d->init();
+	}
+}
+
+inline void GLArea::setDrawableObjectVector(std::shared_ptr<DrawableObjectVector> v)
+{
+	drawList = v;
+}
+
+inline std::shared_ptr<DrawableObjectVector> GLArea::drawableObjectVector()
+{
+	return drawList;
+}
+
+inline std::shared_ptr<const DrawableObjectVector> GLArea::drawableObjectVector() const
+{
+	return drawList;
+}
+
+inline void GLArea::fitScene()
+{
+	Box3d bb = fullBB();
+	Point3d sceneCenter = bb.center();
+	double sceneRadius = bb.diagonal() / 2;
+
+	setSceneCenter(qglviewer::Vec(sceneCenter.x(), sceneCenter.y(), sceneCenter.z()));
+	setSceneRadius(sceneRadius);
+	showEntireScene();
+}
+
+inline void GLArea::draw()
+{
+	for (const DrawableObject* obj : *drawList)
+		obj->draw();
+}
+
+inline uint GLArea::firstVisibleObject() const
+{
+	uint i = 0;
+
+		   // if the current object i is not visible, check the next one
+	while (i < drawList->size() && !drawList->at(i).isVisible())
+		i++;
+
+	return i;
+}
+
+inline Box3d GLArea::fullBB() const
+{
+	Box3d bb(Point3d(-1,-1,-1), Point3d(1,1,1));
+	if (drawList->size() > 0) {
+		uint i = firstVisibleObject();
+
+		if (i < drawList->size()) {
+			Point3d sc = drawList->at(i).center();
+			bb.min() = sc - drawList->at(i).radius();
+			bb.max() = sc + drawList->at(i).radius();
+
+			for (i = i+1; i < drawList->size(); i++) { //rest of the list
+				Point3d sc = drawList->at(i).center();
+				Point3d tmp = sc - drawList->at(i).radius();
+				bb.min() = vcl::min(bb.min(), tmp);
+				tmp = sc + drawList->at(i).radius();
+				bb.max() = vcl::max(bb.max(), tmp);
+			}
+		}
+	}
+	return bb;
+}
+
+} // namespace vcl
 
 #endif // VCL_EXT_QGLVIEWER_GL_AREA_H
