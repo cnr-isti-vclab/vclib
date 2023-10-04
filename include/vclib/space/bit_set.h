0412a72b5db2a6849d28a97018f0198def558aa8
alemuntoni <muntoni.alessandro@gmail.com>
2023-10-04 16:30:50 +0200

    moving definitions in space/array.h, bit_set.h and box.h

--- include/vclib/space/bit_set.h
+++ include/vclib/space/bit_set.h
@@ -31,55 +31,178 @@
 
 namespace vcl {
 
-/******************************************************************************
- *                                Declarations                                *
- ******************************************************************************/
-
 /**
- * @brief The BitSet class allows to treat an integral type as an array of booleans of a guaranteed
- * size.
+ * @brief The BitSet class allows to treat an integral type as an array of
+ * booleans of a guaranteed size.
  *
- * This class is necessary w.r.t. the std::bitset because the realization of the std::bitset class
- * is implementation defined, and the standard does not constraint the size of the bitset itself.
+ * This class is necessary w.r.t. the std::bitset because the realization of the
+ * std::bitset class is implementation defined, and the standard does not
+ * constraint the size of the bitset itself.
  *
  * This class guarantess that the sizeof(BitSet<T>) is equal to sizeof(T).
  *
- * @tparam T: The type to use as a storage for the bits. It must satisfy the std::integral concept.
+ * @tparam T: The type to use as a storage for the bits. It must satisfy the
+ * std::integral concept.
  *
  * @ingroup space
  */
 template<std::integral T>
 class BitSet
 {
-public:
-	BitSet();
+	T bits = static_cast<T>(0);
 
+public:
+	/**
+	 * @brief The number of the bits of the BitSet.
+	 */
 	static constexpr std::size_t SIZE = sizeof(T) * 8;
 
-	constexpr std::size_t size() const;
+	/**
+	 * @brief Empty constructor. All the Bits of the BitSet are set to false.
+	 */
+	BitSet() {}
 
-	bool operator[](uint i) const;
-	BitProxy<T> operator[](uint i);
-	
-	bool at(uint i) const;
-	BitProxy<T> at(uint i);
-	
-	bool all() const;
-	bool any() const;
-	bool none() const;
+	/**
+	 * @brief Returns the number of bits of the BitSet.
+	 * @return the number of bits of the BitSet.
+	 */
+	constexpr std::size_t size() const { return SIZE; }
 
-	BitSet<T> set();
-	BitSet<T> set(uint i, bool b = true);
+	/**
+	 * @brief Returns the i-th bit value of the BitSet
+	 * @param[in] i: the position of the bit
+	 * @return the boolean value of the bit
+	 */
+	bool operator[](uint i) const
+	{
+		assert(i < SIZE);
+		return bits & (1 << i);
+	}
 
-	BitSet<T> reset();
-	BitSet<T> reset(uint i);
+	/**
+	 * @brief Returns a reference of the i-th bit value of the BitSet
+	 * @param[in] i: the position of the bit
+	 * @return a reference of the boolean value of the bit
+	 */
+	BitProxy<T> operator[](uint i)
+	{
+		assert(i < SIZE);
+		return BitProxy(bits, i);
+	}
 
-	BitSet<T> flip();
-	BitSet<T> flip(uint i);
+	/**
+	 * @brief Returns the i-th bit value of the BitSet, with bounds checking.
+	 * @param[in] i: the position of the bit
+	 * @throws std::out_of_range if i is >= BitSet<T>::SIZE
+	 * @return the boolean value of the bit
+	 */
+	bool at(uint i) const
+	{
+		if (i < SIZE)
+			return bits & (1 << i);
+		else
+			throw std::out_of_range(std::to_string(i) + " out of range.");
+	}
 
-private:
-	T bits = static_cast<T>(0);
+	/**
+	 * @brief Returns a reference of the i-th bit value of the BitSet, with
+	 * bounds checking.
+	 * @param[in] i: the position of the bit
+	 * @throws std::out_of_range if i is >= BitSet<T>::SIZE
+	 * @return a reference of the boolean value of the bit
+	 */
+	BitProxy<T> at(uint i)
+	{
+		if (i < SIZE)
+			return BitProxy(bits, i);
+		else
+			throw std::out_of_range(std::to_string(i) + " out of range.");
+	}
+
+	/**
+	 * @brief Returns true if all the bits of the BitSet are set to true.
+	 * @return true if all the bits of the BitSet are set to true.
+	 */
+	bool all() const { return bits == ~static_cast<T>(0); }
+
+	/**
+	 * @brief Returns true if any of the bits of the BitSet are set to true.
+	 * @return true if any of the bits of the BitSet are set to true.
+	 */
+	bool any() const { return !none(); }
+
+	/**
+	 * @brief Returns true if none of the bits of the BitSet is set to true.
+	 * @return true if none of the bits of the BitSet is set to true.
+	 */
+	bool none() const { return bits == static_cast<T>(0); }
+
+	/**
+	 * @brief Sets all the bits to true
+	 * @return `*this`
+	 */
+	BitSet<T> set()
+	{
+		bits = ~static_cast<T>(0);
+		return *this;
+	}
+
+	/**
+	 * @brief Sets the bit at position i to b
+	 * @param[in] i: the position of the bit
+	 * @param[in] b: the boolena value to assign to the bit
+	 * @return `*this`
+	 */
+	BitSet<T> set(uint i, bool b = true)
+	{
+		at(i) = b;
+		return *this;
+	}
+
+	/**
+	 * @brief Sets all the bits to false
+	 * @return `*this`
+	 */
+	BitSet<T> reset()
+	{
+		bits = 0;
+		return *this;
+	}
+
+	/**
+	 * @brief Sets the bit at position i to false
+	 * @param[in] i: the position of the bit
+	 * @return `*this`
+	 */
+	BitSet<T> reset(uint i)
+	{
+		at(i) = false;
+		return *this;
+	}
+
+	/**
+	 * @brief Flips all the bits of the BitSet
+	 * @return `*this`
+	 */
+	BitSet<T> flip()
+	{
+		bits = ~bits;
+		return *this;
+	}
+
+	/**
+	 * @brief Flips the bit at position i
+	 * @param[in] i: the position of the bit
+	 * @return `*this`
+	 */
+	BitSet<T> flip(uint i)
+	{
+		at(i) = !at(i);
+		return *this;
+	}
 };
+
+/* Specialization Aliases */
 
 /**
  * @brief BitSet8 is a BitSet of 8 bits.
@@ -108,182 +231,6 @@ using BitSet32 = BitSet<int>;
  * @ingroup space
  */
 using BitSet64 = BitSet<std::size_t>;
-
-/******************************************************************************
- *                                Definitions                                 *
- ******************************************************************************/
-
-/**
- * @brief Empty constructor. All the Bits of the BitSet are set to false.
- */
-template <std::integral T>
-BitSet<T>::BitSet()
-{
-}
-
-/**
- * @brief Returns the number of bits of the BitSet.
- * @return the number of bits of the BitSet.
- */
-template<std::integral T>
-constexpr std::size_t BitSet<T>::size() const
-{
-	return SIZE;
-}
-
-/**
- * @brief Returns the i-th bit value of the BitSet
- * @param[in] i: the position of the bit
- * @return the boolean value of the bit
- */
-template<std::integral T>
-bool BitSet<T>::operator[](uint i) const
-{
-	assert(i < SIZE);
-	return bits & (1 << i);
-}
-
-/**
- * @brief Returns a reference of the i-th bit value of the BitSet
- * @param[in] i: the position of the bit
- * @return a reference of the boolean value of the bit
- */
-template<std::integral T>
-BitProxy<T> BitSet<T>::operator[](uint i)
-{
-	assert(i < SIZE);
-	return BitProxy(bits, i);
-}
-
-/**
- * @brief Returns the i-th bit value of the BitSet, with bounds checking.
- * @param[in] i: the position of the bit
- * @throws std::out_of_range if i is >= BitSet<T>::SIZE
- * @return the boolean value of the bit
- */
-template<std::integral T>
-bool BitSet<T>::at(uint i) const
-{
-	if (i < SIZE)
-		return bits & (1 << i);
-	else
-		throw std::out_of_range(std::to_string(i) + " out of range.");
-}
-
-/**
- * @brief Returns a reference of the i-th bit value of the BitSet, with bounds checking.
- * @param[in] i: the position of the bit
- * @throws std::out_of_range if i is >= BitSet<T>::SIZE
- * @return a reference of the boolean value of the bit
- */
-template<std::integral T>
-BitProxy<T> BitSet<T>::at(uint i)
-{
-	if (i < SIZE)
-		return BitProxy(bits, i);
-	else
-		throw std::out_of_range(std::to_string(i) + " out of range.");
-}
-
-/**
- * @brief Returns true if all the bits of the BitSet are set to true.
- * @return true if all the bits of the BitSet are set to true.
- */
-template<std::integral T>
-bool BitSet<T>::all() const
-{
-	return bits == ~static_cast<T>(0);
-}
-
-/**
- * @brief Returns true if any of the bits of the BitSet are set to true.
- * @return true if any of the bits of the BitSet are set to true.
- */
-template<std::integral T>
-bool BitSet<T>::any() const
-{
-	return !none();
-}
-
-/**
- * @brief Returns true if none of the bits of the BitSet is set to true.
- * @return true if none of the bits of the BitSet is set to true.
- */
-template<std::integral T>
-bool BitSet<T>::none() const
-{
-	return bits == static_cast<T>(0);
-}
-
-/**
- * @brief Sets all the bits to true
- * @return `*this`
- */
-template<std::integral T>
-BitSet<T> BitSet<T>::set()
-{
-	bits = ~static_cast<T>(0);
-	return *this;
-}
-
-/**
- * @brief Sets the bit at position i to b
- * @param[in] i: the position of the bit
- * @param[in] b: the boolena value to assign to the bit
- * @return `*this`
- */
-template<std::integral T>
-BitSet<T> BitSet<T>::set(uint i, bool b)
-{
-	at(i) = b;
-	return *this;
-}
-
-/**
- * @brief Sets all the bits to false
- * @return `*this`
- */
-template<std::integral T>
-BitSet<T> BitSet<T>::reset()
-{
-	bits = 0;
-	return *this;
-}
-
-/**
- * @brief Sets the bit at position i to false
- * @param[in] i: the position of the bit
- * @return `*this`
- */
-template<std::integral T>
-BitSet<T> BitSet<T>::reset(uint i)
-{
-	at(i) = false;
-	return *this;
-}
-
-/**
- * @brief Flips all the bits of the BitSet
- * @return `*this`
- */
-template<std::integral T>
-BitSet<T> BitSet<T>::flip()
-{
-	bits = ~bits;
-	return *this;
-}
-
-/**
- * @brief Flips the bit at position i
- * @param[in] i: the position of the bit
- * @return `*this`
- */
-template<std::integral T>
-BitSet<T> BitSet<T>::flip(uint i)
-{
-	at(i) = !at(i);
-	return *this;
-}
 
 } // namespace vcl
 
--- include/vclib/space/bit_set/bit_proxy.h
+++ include/vclib/space/bit_set/bit_proxy.h
@@ -8,85 +8,53 @@
 
 namespace vcl {
 
-/******************************************************************************
- *                                Declarations                                *
- ******************************************************************************/
+// See: https://stackoverflow.com/a/10145050/5851101
 
 /**
- * @brief The BitProxy class allows to access to a bool reference from a bit saved in a mask,
- * and then allow assignment.
- *
- * See: https://stackoverflow.com/a/10145050/5851101
+ * @brief The BitProxy class allows to access to a bool reference from a bit
+ * saved in a mask, and then allow assignment.
  *
  * @ingroup space
  */
 template<std::integral T>
 class BitProxy
 {
-public:
-	BitProxy(T& mask, uint index);
-
-	operator bool() const;
-
-	void operator=(bool bit);
-
-	BitProxy& operator|=(bool bit);
-
-	BitProxy& operator&=(bool bit);
-
-	BitProxy& operator/=(bool bit);
-
-private:
 	std::reference_wrapper<T> mask;
 	const uint index;
+
+public:
+	/**
+	 * @brief Constructs the BitProxy with the given mask and index.
+	 * @param[in] mask: the mask containing the bit to access.
+	 * @param[in] index: the index of the bit to access.
+	 */
+	BitProxy(T& mask, uint index) : mask(mask), index(index) {}
+
+	operator bool() const { return mask.get() & (1 << index); }
+
+	void operator=(bool bit)
+	{
+		mask.get() = (mask.get() & ~(bit << index)) | (bit << index);
+	}
+
+	BitProxy& operator|=(bool bit)
+	{
+		mask.get() |= (bit << index);
+		return *this;
+	}
+
+	BitProxy& operator&=(bool bit)
+	{
+		mask.get() &= ~(bit << index);
+		return *this;
+	}
+
+	BitProxy& operator/=(bool bit)
+	{
+		mask.get() ^= (bit << index);
+		return *this;
+	}
 };
-
-/******************************************************************************
- *                                Definitions                                 *
- ******************************************************************************/
-
-/**
- * @brief Constructs the BitProxy with the given mask and index.
- * @param[in] mask: the mask containing the bit to access.
- * @param[in] index: the index of the bit to access.
- */
-template<std::integral T>
-BitProxy<T>::BitProxy(T& mask, uint index) : mask(mask), index(index)
-{
-}
-
-template<std::integral T>
-BitProxy<T>::operator bool() const
-{
-	return mask.get() & (1 << index);
-}
-
-template<std::integral T>
-void BitProxy<T>::operator=(bool bit)
-{
-	mask.get() = (mask.get() & ~(bit << index)) | (bit << index);
-}
-
-template<std::integral T>
-BitProxy<T>& BitProxy<T>::operator|=(bool bit)
-{
-	mask.get() |= (bit << index);
-	return *this;
-}
-
-template<std::integral T>
-BitProxy<T>& BitProxy<T>::operator&=(bool bit)
-{
-	mask.get() &= ~(bit << index);
-	return *this;
-}
-
-template<std::integral T>
-BitProxy<T>& BitProxy<T>::operator/=(bool bit)
-{
-	mask.get() ^= (bit << index);
-	return *this;
-}
 
 } // namespace vcl
 
