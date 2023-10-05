4423d2a4f0ac3e9f568e5cc7ff7046239410008c
Alessandro Muntoni <muntoni.alessandro@gmail.com>
2023-10-05 11:28:51 +0200

    moving definitions in space/grid/regular_grid.h

--- include/vclib/space/grid/regular_grid.h
+++ include/vclib/space/grid/regular_grid.h
@@ -30,64 +30,204 @@
 
 namespace vcl {
 
-/******************************************************************************
- *                                Declarations                                *
- ******************************************************************************/
-
 template<typename Scalar, int N>
 class RegularGrid
 {
+	static_assert(
+		N > 0,
+		"Number of dimensions of the regular grid must be > 1.");
+
 public:
-	static_assert(N > 0, "Number of dimensions of the regular grid must be > 1.");
+	using PointType       = Point<Scalar, N>;
+	using BBoxType        = Box<PointType>;
+
+private:
+	BBoxType       bbox;
+	Point<uint, N> siz;
+
+public:
 	static const int DIM  = N;
 	using ScalarType      = Scalar;
-	using PointType       = Point<Scalar, N>;
 	using CellCoord       = Point<uint, N>;
-	using BBoxType        = Box<PointType>;
 
 	using CellIterator = vcl::CellIterator<N>;
 	using CellView     = vcl::View<CellIterator>;
 
 	RegularGrid() = default;
-	RegularGrid(const Point<Scalar, N>& min, const Point<Scalar, N>& max, const Point<uint, N>& size);
-	RegularGrid(const Box<Point<Scalar, N>>& bbox, const Point<uint, N>& size);
 
-	Point<Scalar, N> min() const;
-	Point<Scalar, N> max() const;
+	RegularGrid(
+		const Point<Scalar, N>& min,
+		const Point<Scalar, N>& max,
+		const Point<uint, N>&   size) :
+			bbox(min, max),
+			siz(size)
+	{
+	}
 
-	Scalar length(uint d) const;
-	Point<Scalar, N> lengths() const;
+	RegularGrid(const Box<Point<Scalar, N>>& bbox, const Point<uint, N>& size) :
+			bbox(bbox), siz(size)
+	{
+	}
 
-	uint cellNumber(uint d) const;
-	Point<uint, N> cellNumbers() const;
+	Point<Scalar, N> min() const { return bbox.min(); }
 
-	uint indexOfCell(const CellCoord& c) const;
-	CellCoord cellOfIndex(uint i) const;
+	Point<Scalar, N> max() const { return bbox.max(); }
 
-	Scalar cellLength(uint d) const;
-	Point<Scalar, N> cellLengths() const;
-	Scalar cellDiagonal() const;
+	/**
+	 * @brief Returns the edge legth of the bounding box of the grid in the d-th
+	 * dimension
+	 * @return
+	 */
+	Scalar length(uint d) const { return bbox.dim(d); }
 
-	uint cell(uint d, const Scalar& s) const;
-	CellCoord cell(const Point<Scalar, N>& p) const;
+	/**
+	 * @brief Returns the edge legths of the bounding box of the grid
+	 * @return
+	 */
+	Point<Scalar, N> lengths() const
+	{
+		Point<Scalar, N> p;
+		for (size_t i = 0; i < DIM; ++i)
+			p(i) = length(i);
+		return p;
+	}
 
-	Point<Scalar, N> cellLowerCorner(const CellCoord& c) const;
+	/**
+	 * @brief Returns the number of cells of the Grid in the d-th dimension
+	 * @param i
+	 * @return
+	 */
+	uint cellNumber(uint d) const { return siz(d); }
 
-	BBoxType cellBox(const CellCoord& c) const;
+	/**
+	 * @brief Returns the number of cells for each dimension of the grid
+	 * @return
+	 */
+	Point<uint, N> cellNumbers() const { return siz; }
 
-	CellIterator cellBegin() const;
-	CellIterator cellBegin(const CellCoord& first, const CellCoord& last) const;
-	CellIterator cellEnd() const;
-	CellView     cells() const;
-	CellView     cells(const CellCoord& first, const CellCoord& last) const;
+	/**
+	 * @brief Return an unique index that can be associated to the given cell
+	 * coordinate.
+	 * @param c
+	 * @return
+	 */
+	uint indexOfCell(const CellCoord& c) const
+	{
+		unsigned long int ind = c[0];
+		assert(c[0] < siz[0]);
+		for (unsigned int i = 1; i < N; i++) {
+			assert(c[i] < siz[i]);
+			ind *= siz[i];
+			ind += c[i];
+		}
+		return ind;
+	}
+
+	/**
+	 * @brief Returns the cell coordinate associated to the given unique index.
+	 * @param index
+	 * @return
+	 */
+	CellCoord cellOfIndex(uint i) const
+	{
+		typename RegularGrid<Scalar, N>::CellCoord c;
+		for (long int i = N - 1; i >= 0; i--) {
+			c[i] = index % siz[i];
+			index /= siz[i];
+		}
+		return c;
+	}
+
+	/**
+	 * @brief Returns the length of a cell of the grid in the d-th dimension
+	 * @param i
+	 * @return
+	 */
+	Scalar cellLength(uint d) const
+	{
+		return length(d)/cellNumber(d);
+	}
+
+	/**
+	 * @brief Returns the lengths of a cell of the grid for each dimension
+	 * @return
+	 */
+	Point<Scalar, N> cellLengths() const
+	{
+		Point<Scalar, N> p;
+		for (size_t i = 0; i < DIM; ++i)
+			p(i) = cellLength(i);
+		return p;
+	}
+
+	Scalar cellDiagonal() const
+	{
+		return cellLengths().norm();
+	}
+
+	uint cell(uint d, const Scalar& s) const
+	{
+		if (s < bbox.min()(d)) return 0;
+		if (s > bbox.max()(d)) return cellNumber(d) - 1;
+		Scalar t = s - bbox.min()(d);
+		return uint(t / cellLength(d));
+	}
+
+	CellCoord cell(const Point<Scalar, N>& p) const
+	{
+		CellCoord c;
+		for (size_t i = 0; i < DIM; ++i)
+			c(i) = cell(i, p(i));
+		return c;
+	}
+
+	Point<Scalar, N> cellLowerCorner(const CellCoord& c) const
+	{
+		Point<Scalar, N> l;
+		for (size_t i = 0; i < DIM; ++i)
+			l(i) = min(i) + c(i) * cellLength(i);//cellLowerCorner(i, c(i));
+		return l;
+	}
+
+	BBoxType cellBox(const CellCoord& c) const
+	{
+		BBoxType b;
+
+		Point<Scalar, N> p;
+		for (size_t i = 0; i < DIM; ++i)
+			p(i) = (Scalar)c(i)*cellLength(i);
+		p += bbox.min();
+		b.min() = p;
+		b.max() = (p + cellLengths());
+
+		return b;
+	}
+
+	CellIterator cellBegin() const { return CellIterator(CellCoord(), siz); }
+
+	CellIterator cellBegin(const CellCoord& first, const CellCoord& last) const
+	{
+		return CellIterator(first, last + 1);
+	}
+
+	CellIterator cellEnd() const { return CellIterator(); }
+
+	CellView cells() const { return CellView(cellBegin(), cellEnd()); }
+
+	CellView cells(const CellCoord& first, const CellCoord& last) const
+	{
+		return CellView(cellBegin(first, last), cellEnd());
+	}
 
 protected:
-	void set(const Box<Point<Scalar, N>>& box, const Point<uint, N>& size);
-
-private:
-	BBoxType       bbox;
-	Point<uint, N> siz;
+	void set(const Box<Point<Scalar, N>>& box, const Point<uint, N>& size)
+	{
+		bbox = box;
+		siz = size;
+	}
 };
+
+/* Specialization Aliases */
 
 template<typename Scalar>
 using RegularGrid2 = RegularGrid<Scalar, 2>;
@@ -95,244 +235,26 @@ using RegularGrid2 = RegularGrid<Scalar, 2>;
 template<typename Scalar>
 using RegularGrid3 = RegularGrid<Scalar, 3>;
 
-// deduction guides
+/* Deduction guides */
+
 template<PointConcept PointType, typename D>
-RegularGrid(PointType, PointType, D) -> RegularGrid<typename PointType::ScalarType, PointType::DIM>;
+RegularGrid(PointType, PointType, D)
+	-> RegularGrid<typename PointType::ScalarType, PointType::DIM>;
 
-template<PointConcept PointType>
-vcl::Point<uint, PointType::DIM> bestGridSize(const PointType& lengths, uint nElements);
-
-/******************************************************************************
- *                                Definitions                                 *
- ******************************************************************************/
-
-template<typename Scalar, int N>
-RegularGrid<Scalar, N>::RegularGrid(
-	const Point<Scalar, N>& min,
-	const Point<Scalar, N>& max,
-	const Point<uint, N>&   size) :
-		bbox(min, max), siz(size)
-{
-}
-
-template<typename Scalar, int N>
-RegularGrid<Scalar, N>::RegularGrid(const Box<Point<Scalar, N> >& bbox, const Point<uint, N>& size) :
-		bbox(bbox), siz(size)
-{
-}
-
-template<typename Scalar, int N>
-Point<Scalar, N> RegularGrid<Scalar, N>::min() const
-{
-	return bbox.min();
-}
-
-template<typename Scalar, int N>
-Point<Scalar, N> RegularGrid<Scalar, N>::max() const
-{
-	return bbox.max();
-}
+/* Regular Grid related functions */
 
 /**
- * @brief Returns the edge legth of the bounding box of the grid in the d-th
- * dimension
- * @return
- */
-template<typename Scalar, int N>
-Scalar RegularGrid<Scalar, N>::length(uint d) const
-{
-	return bbox.dim(d);
-}
-
-/**
- * @brief Returns the edge legths of the bounding box of the grid
- * @return
- */
-template<typename Scalar, int N>
-Point<Scalar, N> RegularGrid<Scalar, N>::lengths() const
-{
-	Point<Scalar, N> p;
-	for (size_t i = 0; i < DIM; ++i)
-		p(i) = length(i);
-	return p;
-}
-
-/**
- * @brief Returns the number of cells of the Grid in the d-th dimension
- * @param i
- * @return
- */
-template<typename Scalar, int N>
-uint RegularGrid<Scalar, N>::cellNumber(uint d) const
-{
-	return siz(d);
-}
-
-/**
- * @brief Returns the number of cells for each dimension of the grid
- * @return
- */
-template<typename Scalar, int N>
-Point<uint, N> RegularGrid<Scalar, N>::cellNumbers() const
-{
-	return siz;
-}
-
-/**
- * @brief Return an unique index that can be associated to the given cell coordinate.
- * @param c
- * @return
- */
-template<typename Scalar, int N>
-uint RegularGrid<Scalar, N>::indexOfCell(const CellCoord& c) const
-{
-	unsigned long int ind = c[0];
-	assert(c[0] < siz[0]);
-	for (unsigned int i = 1; i < N; i++) {
-		assert(c[i] < siz[i]);
-		ind *= siz[i];
-		ind += c[i];
-	}
-	return ind;
-}
-
-/**
- * @brief Returns the cell coordinate associated to the given unique index.
- * @param index
- * @return
- */
-template<typename Scalar, int N>
-typename RegularGrid<Scalar, N>::CellCoord RegularGrid<Scalar, N>::cellOfIndex(uint index) const
-{
-	typename RegularGrid<Scalar, N>::CellCoord c;
-	for (long int i = N - 1; i >= 0; i--) {
-		c[i] = index % siz[i];
-		index /= siz[i];
-	}
-	return c;
-}
-
-/**
- * @brief Returns the length of a cell of the grid in the d-th dimension
- * @param i
- * @return
- */
-template<typename Scalar, int N>
-Scalar RegularGrid<Scalar, N>::cellLength(uint d) const
-{
-	return length(d)/cellNumber(d);
-}
-
-/**
- * @brief Returns the lengths of a cell of the grid for each dimension
- * @return
- */
-template<typename Scalar, int N>
-Point<Scalar, N> RegularGrid<Scalar, N>::cellLengths() const
-{
-	Point<Scalar, N> p;
-	for (size_t i = 0; i < DIM; ++i)
-		p(i) = cellLength(i);
-	return p;
-}
-
-template<typename Scalar, int N>
-Scalar RegularGrid<Scalar, N>::cellDiagonal() const
-{
-	return cellLengths().norm();
-}
-
-template<typename Scalar, int N>
-uint RegularGrid<Scalar, N>::cell(uint d, const Scalar& s) const
-{
-	if (s < bbox.min()(d)) return 0;
-	if (s > bbox.max()(d)) return cellNumber(d) - 1;
-	Scalar t = s - bbox.min()(d);
-	return uint(t / cellLength(d));
-}
-
-template<typename Scalar, int N>
-Point<uint, N> RegularGrid<Scalar, N>::cell(const Point<Scalar, N>& p) const
-{
-	CellCoord c;
-	for (size_t i = 0; i < DIM; ++i)
-		c(i) = cell(i, p(i));
-	return c;
-}
-
-template<typename Scalar, int N>
-Point<Scalar, N> RegularGrid<Scalar, N>::cellLowerCorner(const CellCoord& c) const
-{
-	Point<Scalar, N> l;
-	for (size_t i = 0; i < DIM; ++i)
-		l(i) = min(i) + c(i) * cellLength(i);//cellLowerCorner(i, c(i));
-	return l;
-}
-
-template<typename Scalar, int N>
-typename RegularGrid<Scalar, N>::BBoxType RegularGrid<Scalar, N>::cellBox(const CellCoord& c) const
-{
-	BBoxType b;
-
-	Point<Scalar, N> p;
-	for (size_t i = 0; i < DIM; ++i)
-		p(i) = (Scalar)c(i)*cellLength(i);
-	p += bbox.min();
-	b.min() = p;
-	b.max() = (p + cellLengths());
-
-	return b;
-}
-
-template<typename Scalar, int N>
-typename RegularGrid<Scalar, N>::CellIterator RegularGrid<Scalar, N>::cellBegin() const
-{
-	return CellIterator(CellCoord(), siz);
-}
-
-template<typename Scalar, int N>
-typename RegularGrid<Scalar, N>::CellIterator
-RegularGrid<Scalar, N>::cellBegin(const CellCoord& first, const CellCoord& last) const
-{
-	return CellIterator(first, last + 1);
-}
-
-template<typename Scalar, int N>
-typename RegularGrid<Scalar, N>::CellIterator RegularGrid<Scalar, N>::cellEnd() const
-{
-	return CellIterator();
-}
-
-template<typename Scalar, int N>
-typename RegularGrid<Scalar, N>::CellView RegularGrid<Scalar, N>::cells() const
-{
-	return CellView(cellBegin(), cellEnd());
-}
-
-template<typename Scalar, int N>
-typename RegularGrid<Scalar, N>::CellView
-RegularGrid<Scalar, N>::cells(const CellCoord& first, const CellCoord& last) const
-{
-	return CellView(cellBegin(first, last), cellEnd());
-}
-
-template<typename Scalar, int N>
-void RegularGrid<Scalar, N>::set(const Box<Point<Scalar, N> >& box, const Point<uint, N>& size)
-{
-	bbox = box;
-	siz = size;
-}
-
-/**
- * @brief Returns the best sizes (number of cells per dimension) of a Grid, starting from the
- * lengths of the grid and the number of elements to place in the grid.
+ * @brief Returns the best sizes (number of cells per dimension) of a Grid,
+ * starting from the lengths of the grid and the number of elements to place in
+ * the grid.
  *
  * @param lengths
  * @param nElements
  * @return
  */
 template<PointConcept PointType>
-Point<uint, PointType::DIM> bestGridSize(const PointType& lengths, uint nElements)
+Point<uint, PointType::DIM>
+bestGridSize(const PointType& lengths, uint nElements)
 {
 	using Scalar = PointType::ScalarType;
 
@@ -361,8 +283,7 @@ Point<uint, PointType::DIM> bestGridSize(const PointType& lengths, uint nElement
 	uint greaterEpsDimNumber = DIM - lessEpsDimNumber;
 
 	if (nElements > 0 && sanityCheckLengths) {
-
-			   // no lenghts less than epsilon - standard computation for sizes
+		// no lenghts less than epsilon - standard computation for sizes
 		if (greaterEpsDimNumber == DIM) {
 			Scalar product = 1;
 			for (uint i = 0; i < DIM; i++)
@@ -380,16 +301,17 @@ Point<uint, PointType::DIM> bestGridSize(const PointType& lengths, uint nElement
 					sizes(i) = 1;
 				}
 				else {
-					// compute the product between all the dimensions that are not i and not less
-					// than epsilon
+					// compute the product between all the dimensions that are
+					// not i and not less than epsilon
 					Scalar product = 1;
 					for (uint j = 0; j < DIM; j++)
 						if (j != i && !isLessEps[j])
 							product *= lengths(j);
 
-						   // ith dimension size
-					sizes(i) =
-						std::pow((ncell * lengths(i) / product), (1.0 / greaterEpsDimNumber));
+					// ith dimension size
+					sizes(i) = std::pow(
+						(ncell * lengths(i) / product),
+						(1.0 / greaterEpsDimNumber));
 				}
 			}
 		}
@@ -400,6 +322,8 @@ Point<uint, PointType::DIM> bestGridSize(const PointType& lengths, uint nElement
 
 	return sizes;
 }
+
+
 
 } // namespace vcl
 
