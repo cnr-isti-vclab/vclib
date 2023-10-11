d91ae4fd657e2ba9c93599411ce9fddebbe66225
Alessandro Muntoni <muntoni.alessandro@gmail.com>
2023-10-11 14:03:29 +0200

    template element_requirements generates only an header

--- scripts/templates/mesh/requirements/element_requirements.h
+++ scripts/templates/mesh/requirements/element_requirements.h
@@ -4,8 +4,10 @@
 #define VCL_MESH_REQUIREMENTS_%EL_U%_REQUIREMENTS_H
 
 #include <vclib/concepts/mesh.h>
+#include <vclib/exceptions/mesh_exceptions.h>
 
 #include "element_requirements.h"
+#include "../containers/%EL%_container.h"
 
 /**
  * @defgroup %EL%_requirements %EL_UC% Requirements
@@ -23,22 +25,56 @@ namespace vcl {
 
 // %EL_UC% Compactness
 
+/**
+ * @brief Returns `true` if the given mesh has its %EL_UC%Container compact.
+ *
+ * @tparam MeshType: the type of the Mesh to check, it must satisfy
+ * the %EL_UC%MeshConcept.
+ *
+ * @param m: the mesh on which check the compactness of the 
+ * %EL_UC%Container.
+ * @return `true` if the %EL_UC%Container is compact.
+ *
+ * @ingroup %EL%_requirements
+ */
 template<%EL_UC%MeshConcept MeshType>
-bool is%EL_UC%ContainerCompact(const MeshType&);
+bool is%EL_UC%ContainerCompact(const MeshType& m)
+{
+	return isElementContainerCompact<%EL_U%>(m);
+}
 
-%IS_ENABLE_COMPONENTS_HEADER%
+%IS_ENABLE_COMPONENTS%
 /*********************
  * require functions *
  *********************/
 
 // %EL_UC% Compactness
 
+/**
+ * @brief This function asserts that a Mesh has the %EL_UC%Container 
+ * compact (no elements flagged as deleted).
+ *
+ * If the %EL_UC%Container is not compact, a
+ * vcl::MissingCompactnessException will be thrown.
+ *
+ * @tparam MeshType: the type of the Mesh to check, it must satisfy the
+ * %EL_UC%MeshConcept.
+ *
+ * @throws vcl::MissingCompactnessException if the %EL_UC%Container
+ * is not compact.
+ *
+ * @param[in] m: the mesh on which check the compactness of the
+ * %EL_UC%Container.
+ *
+ * @ingroup %EL%_requirements
+ */
 template<%EL_UC%MeshConcept MeshType>
-void require%EL_UC%ContainerCompactness(const MeshType& m);
+void require%EL_UC%ContainerCompactness(const MeshType& m)
+{
+	requireElementContainerCompactness<%EL_U%>(m);
+}
 
-%REQUIRE_COMPONENTS_HEADER%
+%REQUIRE_COMPONENTS%
 } // namespace vcl
-
-#include "%EL%_requirements.cpp"
 
 #endif // VCL_MESH_REQUIREMENTS_%EL_U%_REQUIREMENTS_H
