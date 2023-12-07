5ddc426c17bfdb9d37dda8d6571e5b7c1d3b9248
Alessandro Muntoni <alemuntoni@users.noreply.github.com>
2023-12-07 09:18:41 +0100

    replace headers in cmake files and tests

--- cmake/vclib_common_settings.cmake
+++ cmake/vclib_common_settings.cmake
@@ -3,10 +3,21 @@
 #* Visual Computing Library                                                  *
 #*                                                                           *
 #* Copyright(C) 2021-2023                                                    *
-#* Alessandro Muntoni                                                        *
-#* VCLab - ISTI - Italian National Research Council                          *
+#* Visual Computing Lab                                                      *
+#* ISTI - Italian National Research Council                                  *
 #*                                                                           *
 #* All rights reserved.                                                      *
+#*                                                                           *
+#* This program is free software; you can redistribute it and/or modify      *
+#* it under the terms of the GNU General Public License as published by      *
+#* the Free Software Foundation; either version 3 of the License, or         *
+#* (at your option) any later version.                                       *
+#*                                                                           *
+#* This program is distributed in the hope that it will be useful,           *
+#* but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+#* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              *
+#* GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
+#* for more details.                                                         *
 #****************************************************************************/
 
 ### Build settings
--- scripts/replace_headers.py
+++ scripts/replace_headers.py
@@ -10,12 +10,12 @@ def replace_first_between(s, start, end, replacement):
     except ValueError:
         return s
 
-def replace_header(file_path, header_string):
+def replace_header(file_path, header_string, start='/*', end='*/'):
     # Read in the file
     with open(file_path, 'r') as file :
         file_string = file.read()
 
-    file_string = replace_first_between(file_string, '/*', '*/', header_string)
+    file_string = replace_first_between(file_string, start, end, header_string)
 
     with open(file_path, 'w') as file:
         file.write(file_string)
@@ -34,5 +34,26 @@ def replace_headers_in_dir(folder_path):
         elif file_path.endswith(('.h', '.cpp')):
             replace_header(file_path, header_string)
 
+def replace_cmake_headers_in_dir(folder_path, recursive=True):
+    # get the path where this script is located
+    path = os.path.dirname(os.path.abspath(__file__))
+
+    # Read in the file
+    with open(path + '/templates/header.txt', 'r') as file :
+        header_string = file.read()
+        # replace the first char of the line with a #
+        header_string = re.sub(r'^.', '#', header_string, flags=re.MULTILINE)
+
+    for file_path in glob.glob(os.path.join(folder_path, '*')):
+        if os.path.isdir(file_path) and recursive:
+            replace_cmake_headers_in_dir(file_path)
+        elif file_path.endswith(('CMakeLists.txt', '.cmake')):
+            replace_header(file_path, header_string, start='#*')
+
 if __name__ == "__main__":
     replace_headers_in_dir('../include/vclib/')
+    replace_headers_in_dir('../test/')
+    replace_cmake_headers_in_dir('../include/vclib/')
+    replace_cmake_headers_in_dir('../external/', recursive=False)
+    replace_cmake_headers_in_dir('../cmake/')
+    replace_cmake_headers_in_dir('../test/')
