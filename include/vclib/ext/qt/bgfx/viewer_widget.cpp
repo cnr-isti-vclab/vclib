9cbfbfc2f00bbb37fa95e6b8f14c3dcef2eead3a
alemuntoni <alemuntoni@users.noreply.github.com>
2024-01-05 10:44:37 +0100

    first draft of viewer_widget

---
+++ include/vclib/ext/qt/bgfx/viewer_widget.cpp
@@ -0,0 +1,39 @@
+/*****************************************************************************
+ * VCLib                                                                     *
+ * Visual Computing Library                                                  *
+ *                                                                           *
+ * Copyright(C) 2021-2024                                                    *
+ * Visual Computing Lab                                                      *
+ * ISTI - Italian National Research Council                                  *
+ *                                                                           *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This program is free software; you can redistribute it and/or modify      *
+ * it under the terms of the GNU General Public License as published by      *
+ * the Free Software Foundation; either version 3 of the License, or         *
+ * (at your option) any later version.                                       *
+ *                                                                           *
+ * This program is distributed in the hope that it will be useful,           *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              *
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
+ * for more details.                                                         *
+ ****************************************************************************/
+
+#include "viewer_widget.h"
+#include "ui_viewer_widget.h"
+
+namespace vcl::qbgf {
+
+ViewerWidget::ViewerWidget(QWidget* parent) :
+        QWidget(parent), ui(new Ui::ViewerWidget)
+{
+    ui->setupUi(this);
+}
+
+ViewerWidget::~ViewerWidget()
+{
+    delete ui;
+}
+
+} // namespace vcl::qbgf
