f89cfb7e1845187cf82530c252c7b9a632a36e09
alemuntoni <alemuntoni@users.noreply.github.com>
2024-01-22 17:14:29 +0100

    possibility to set size to drawable axis

--- include/vclib/ext/bgfx/drawable_axis.h
+++ include/vclib/ext/bgfx/drawable_axis.h
@@ -40,17 +40,17 @@ class DrawableAxis : public DrawableObject
 {
     bool visible = false;
 
-    vcl::Color colors[3] = {
+    const vcl::Color colors[3] = {
         vcl::Color::Red,
         vcl::Color::Green,
         vcl::Color::Blue};
 
     vcl::Matrix44f matrices[3] = {
-        vcl::Matrix44f::Identity(),
-        vcl::Matrix44f::Identity(),
-        vcl::Matrix44f::Identity()};
+        vcl::Matrix44f::Zero(),
+        vcl::Matrix44f::Zero(),
+        vcl::Matrix44f::Zero()};
 
-    MeshRenderBuffers<vcl::TriMesh> mrbArrow;
+    MeshRenderBuffers<vcl::TriMesh> mrbArrow[2];
 
     bgfx::ProgramHandle program = BGFX_INVALID_HANDLE;
 
@@ -61,25 +61,19 @@ class DrawableAxis : public DrawableObject
     DrawableAxisUniforms uniforms;
 
 public:
-    DrawableAxis()
+    DrawableAxis(double size = 1, bool fromOrigin = false)
     {
-        vcl::TriMesh arrow = vcl::createCylinder<vcl::TriMesh>(0.02, 2);
-        vcl::updatePerVertexNormals(arrow);
+        createAxis(fromOrigin);
 
-        vcl::TriMesh cone = vcl::createCone<vcl::TriMesh>(0.05, 0, 0.2);
-        vcl::updatePerVertexNormals(cone);
-        vcl::translate(cone, vcl::Point3d(0, 1, 0));
-        arrow.append(cone);
-
-        mrbArrow = MeshRenderBuffers<vcl::TriMesh>(
-            arrow,
-            MeshRenderBuffers<vcl::TriMesh>::VERT_NORMALS |
-                MeshRenderBuffers<vcl::TriMesh>::TRIANGLES);
-
-        updateMatrices();
+        updateMatrices(size);
     }
 
     ~DrawableAxis() = default;
+
+    void setSize(double size)
+    {
+        updateMatrices(size);
+    }
 
     void setShaderProgram(const DrawableAxisShaderProgram& sp)
     {
@@ -92,21 +86,25 @@ public:
     {
         if (isVisible()) {
             if (bgfx::isValid(program)) {
+                uint64_t state = 0 | BGFX_STATE_WRITE_RGB |
+                                 BGFX_STATE_WRITE_A | BGFX_STATE_WRITE_Z |
+                                 BGFX_STATE_DEPTH_TEST_LEQUAL |
+                                 BGFX_STATE_MSAA;
                 for (uint i = 0; i < 3; i++) {
-                    uniforms.setColor(colors[i]);
-                    uniforms.bind();
+                    for (uint j = 0; j < 2; j++) {
+                        if (j == 0) // cylinders
+                            uniforms.setColor(colors[i]);
+                        else // rest (cone, spheres...)
+                            uniforms.setColor(vcl::Color::White);
+                        uniforms.bind();
 
-                    mrbArrow.bindVertexBuffers();
-                    mrbArrow.bindIndexBuffers();
+                        mrbArrow[j].bindVertexBuffers();
+                        mrbArrow[j].bindIndexBuffers();
 
-                    bgfx::setTransform(matrices[i].data());
+                        bgfx::setTransform(matrices[i].data());
 
-                    uint64_t state = 0 | BGFX_STATE_WRITE_RGB |
-                                     BGFX_STATE_WRITE_A | BGFX_STATE_WRITE_Z |
-                                     BGFX_STATE_DEPTH_TEST_LEQUAL |
-                                     BGFX_STATE_MSAA;
-
-                    bgfx::submit(0, program);
+                        bgfx::submit(0, program);
+                    }
                 }
             }
         }
@@ -123,17 +121,85 @@ public:
     void setVisibility(bool vis) { visible = vis; }
 
 private:
-    void updateMatrices()
+    void updateMatrices(double size)
     {
-        matrices[0](0,0) = 0;
-        matrices[0](0,1) = 1;
-        matrices[0](1,0) = -1;
-        matrices[0](1,1) = 0;
+        matrices[0](0, 1) = size;
+        matrices[0](1, 0) = -size;
+        matrices[0](2, 2) = size;
+        matrices[0](3, 3) = 1;
 
-        matrices[2](1,1) = 0;
-        matrices[2](1,2) = -1;
-        matrices[2](2,1) = 1;
-        matrices[2](2,2) = 0;
+        matrices[1](0, 0) = size;
+        matrices[1](1, 1) = size;
+        matrices[1](2, 2) = size;
+        matrices[1](3, 3) = 1;
+
+        matrices[2](0, 0) = size;
+        matrices[2](1, 2) = -size;
+        matrices[2](2, 1) = size;
+        matrices[2](3, 3) = 1;
+    }
+
+    void createAxis(bool fromOrigin)
+    {
+        const double unitLength = 1.0;
+
+        const double cylLength = fromOrigin ? unitLength : unitLength * 2;
+        const double cylRadius = cylLength * 0.0025;
+
+        const double coneRadius = cylRadius * 10;
+        const double coneLength = cylLength * 0.1;
+
+        const double firstSphereRadius = unitLength * 0.02;
+        const double commonSphereRadius = unitLength * 0.008;
+
+        vcl::TriMesh cylinder =
+            vcl::createCylinder<vcl::TriMesh>(cylRadius, cylLength);
+        if (fromOrigin) {
+            vcl::translate(cylinder, vcl::Point3d(0, unitLength * 0.5, 0));
+        }
+
+        vcl::updatePerVertexNormals(cylinder);
+
+        vcl::TriMesh rest =
+            vcl::createCone<vcl::TriMesh>(coneRadius, 0, coneLength);
+        double transl = unitLength + (coneLength * 0.5);
+        vcl::translate(rest, vcl::Point3d(0, transl, 0));
+
+        if (!fromOrigin) {
+            vcl::Sphered s(vcl::Point3d(0, -1, 0), firstSphereRadius);
+            vcl::TriMesh sp = vcl::createSphere<vcl::TriMesh>(s);
+            rest.append(sp);
+        }
+
+        for (uint i = 0; i < 9; ++i) {
+            const double step = unitLength * 0.1;
+            const double x = step + i * step;
+            vcl::Sphered s(vcl::Point3d(0, x, 0), commonSphereRadius);
+            vcl::TriMesh sp = vcl::createSphere<vcl::TriMesh>(s);
+            rest.append(sp);
+            if (!fromOrigin) {
+                s.center().y() = -x;
+                sp = vcl::createSphere<vcl::TriMesh>(s);
+                rest.append(sp);
+            }
+        }
+
+        const double rad = fromOrigin ? firstSphereRadius : commonSphereRadius;
+        vcl::Sphered s = vcl::Sphered(vcl::Point3d(0, 0, 0), rad);
+        vcl::TriMesh sp = vcl::createSphere<vcl::TriMesh>(s);
+        rest.append(sp);
+
+        vcl::updatePerVertexNormals(rest);
+
+        mrbArrow[0] = MeshRenderBuffers<vcl::TriMesh>(
+            cylinder,
+            MeshRenderBuffers<vcl::TriMesh>::VERT_NORMALS |
+                MeshRenderBuffers<vcl::TriMesh>::TRIANGLES);
+
+        mrbArrow[1] = MeshRenderBuffers<vcl::TriMesh>(
+            rest,
+            MeshRenderBuffers<vcl::TriMesh>::VERT_NORMALS |
+                MeshRenderBuffers<vcl::TriMesh>::TRIANGLES);
     }
 };
 
