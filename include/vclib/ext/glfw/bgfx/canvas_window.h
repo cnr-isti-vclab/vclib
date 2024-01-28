1dc9c731f2776795357d8dfc34543af8cf3745de
alemuntoni <alemuntoni@users.noreply.github.com>
2024-01-28 18:42:49 +0100

    add glfw canvas window constructor

--- include/vclib/ext/glfw/bgfx/canvas_window.h
+++ include/vclib/ext/glfw/bgfx/canvas_window.h
@@ -23,15 +23,70 @@
 #ifndef VCL_EXT_GLFW_BGFX_CANVAS_WINDOW_H
 #define VCL_EXT_GLFW_BGFX_CANVAS_WINDOW_H
 
+#include <iostream>
+
 #include <vclib/ext/bgfx/canvas.h>
 
+#if BX_PLATFORM_LINUX
+#define GLFW_EXPOSE_NATIVE_X11
+#elif BX_PLATFORM_WINDOWS
+#define GLFW_EXPOSE_NATIVE_WIN32
+#elif BX_PLATFORM_OSX
+#define GLFW_EXPOSE_NATIVE_COCOA
+#endif
 #include <GLFW/glfw3.h>
 
 namespace vcl::bglfwx {
 
+namespace detail {
+
+static void glfwErrorCallback(int error, const char *description)
+{
+    std::cerr<< "GLFW error: "  << error << ": "  << description<< std::endl;
+}
+
+} // namespace detail
+
 class CanvasWindow : public vcl::bgf::Canvas
 {
     using Canvas = vcl::bgf::Canvas;
+
+    GLFWwindow* window = nullptr;
+
+    uint w = 1024, h = 768;
+
+public:
+    explicit CanvasWindow(bgfx::RendererType::Enum renderType = bgfx::RendererType::Count)
+    {
+        glfwSetErrorCallback(detail::glfwErrorCallback);
+        if (!glfwInit()) {
+            std::cerr << "Failed to initialize GLFW" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+
+        window = glfwCreateWindow(w, h, "VCL", nullptr, nullptr);
+        if (!window) {
+            std::cerr << "Failed to create GLFW window" << std::endl;
+            glfwTerminate();
+            exit(EXIT_FAILURE);
+        }
+
+        void* ndt = nullptr;
+        void* nwh = nullptr;
+
+#if BX_PLATFORM_LINUX || BX_PLATFORM_BSD
+        ndt = glfwGetX11Display();
+        nwh = (void*)(uintptr_t)glfwGetX11Window(window);
+#elif BX_PLATFORM_OSX
+        nwh = glfwGetCocoaWindow(window);
+#elif BX_PLATFORM_WINDOWS
+        nwh = glfwGetWin32Window(window);
+#endif
+        Canvas::init(nwh, w, h, ndt, renderType);
+    }
 };
 
 } // namespace vcl::bglfwx
