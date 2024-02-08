5fefb702aa046bde8e83cc8dc87cd34b9b0060e1
Alessandro Muntoni <alemuntoni@users.noreply.github.com>
2024-02-08 11:39:47 +0100

    add mesh_check file

---
+++ include/vclib/mesh/utils/mesh_check.h
@@ -0,0 +1,38 @@
+/*****************************************************************************
+ * VCLib                                                                     *
+ * Visual Computing Library                                                  *
+ *                                                                           *
+ * Copyright(C) 2021-2024                                                    *
+ * Visual Computing Lab                                                      *
+ * ISTI - Italian National Research Council                                  *
+ *                                                                           *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This program is free software; you can redistribute it and/or modify      *
+ * it under the terms of the GNU General Public License as published by      *
+ * the Free Software Foundation; either version 3 of the License, or         *
+ * (at your option) any later version.                                       *
+ *                                                                           *
+ * This program is distributed in the hope that it will be useful,           *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              *
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
+ * for more details.                                                         *
+ ****************************************************************************/
+
+#ifndef VCL_MESH_UTILS_MESH_CHECK_H
+#define VCL_MESH_UTILS_MESH_CHECK_H
+
+#include <vclib/concepts/mesh.h>
+
+namespace vcl {
+
+template<MeshConcept MeshType>
+void checkMeshPointers(const MeshType& mesh)
+{
+
+}
+
+} // namespace vcl
+
+#endif // VCL_MESH_UTILS_MESH_CHECK_H
