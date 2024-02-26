73d3f02fc066380b4102559e75656b1df0fad13c
Alessandro Muntoni <alemuntoni@users.noreply.github.com>
2024-02-26 14:36:27 +0100

    update headers for MPL2 license

--- include/vclib/concepts/mesh/edge_mesh_concept.h
+++ include/vclib/concepts/mesh/edge_mesh_concept.h
@@ -9,15 +9,15 @@
  * All rights reserved.                                                      *
  *                                                                           *
  * This program is free software; you can redistribute it and/or modify      *
- * it under the terms of the GNU General Public License as published by      *
- * the Free Software Foundation; either version 3 of the License, or         *
+ * it under the terms of the Mozilla Public License Version 2.0 as published *
+ * by the Mozilla Foundation; either version 2 of the License, or            *
  * (at your option) any later version.                                       *
  *                                                                           *
  * This program is distributed in the hope that it will be useful,           *
  * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              *
- * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
- * for more details.                                                         *
+ * Mozilla Public License Version 2.0                                        *
+ * (https://www.mozilla.org/en-US/MPL/2.0/) for more details.                *
  ****************************************************************************/
 
 #ifndef VCL_CONCEPTS_MESH_EDGE_MESH_CONCEPT_H
