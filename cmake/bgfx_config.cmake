f68cfa08ee06a49c366250fec6d3c8498e830c79
Alessandro Muntoni <alemuntoni@users.noreply.github.com>
2024-02-27 15:40:32 +0100

    add copyright to cmake files

--- cmake/bgfx_config.cmake
+++ cmake/bgfx_config.cmake
@@ -1,3 +1,25 @@
+#*****************************************************************************
+#* VCLib                                                                     *
+#* Visual Computing Library                                                  *
+#*                                                                           *
+#* Copyright(C) 2021-2024                                                    *
+#* Visual Computing Lab                                                      *
+#* ISTI - Italian National Research Council                                  *
+#*                                                                           *
+#* All rights reserved.                                                      *
+#*                                                                           *
+#* This program is free software; you can redistribute it and/or modify      *
+#* it under the terms of the Mozilla Public License Version 2.0 as published *
+#* by the Mozilla Foundation; either version 2 of the License, or            *
+#* (at your option) any later version.                                       *
+#*                                                                           *
+#* This program is distributed in the hope that it will be useful,           *
+#* but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+#* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              *
+#* Mozilla Public License Version 2.0                                        *
+#* (https://www.mozilla.org/en-US/MPL/2.0/) for more details.                *
+#****************************************************************************/
+
 include(${VCLIB_BGFX_DIR}/cmake/bgfxToolUtils.cmake)
 
 # FILE: the absolute path of the shader file
@@ -122,8 +144,6 @@ endfunction()
 # Note: if the path to the shader begins with "shaders/", it will be removed
 # (the same is done in the "bgf::loadProgram" function)
 function(target_add_bgfx_shaders target_name)
-    list(GET ARGV 0 target_name)
-
     list(REMOVE_AT ARGV 0)
 
     foreach(SHADER ${ARGV})
