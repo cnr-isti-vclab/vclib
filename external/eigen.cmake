ff297eadcec906415d5e127066880cd4e37118b7
Alessandro Muntoni <alemuntoni@users.noreply.github.com>
2024-03-05 16:40:16 +0100

    using eigen target for cmake config

--- external/eigen.cmake
+++ external/eigen.cmake
@@ -24,20 +24,18 @@ find_package(Eigen3 QUIET)
 
 set(VCLIB_EIGEN_DIR ${CMAKE_CURRENT_LIST_DIR}/eigen-3.4.0)
 
-if(VCLIB_ALLOW_SYSTEM_EIGEN AND EIGEN3_INCLUDE_DIR)
+if(VCLIB_ALLOW_SYSTEM_EIGEN AND TARGET Eigen3::Eigen)
     message(STATUS "- Eigen - using system-provided library")
-    set(EIGEN_INCLUDE_DIRS ${EIGEN3_INCLUDE_DIR})
+    add_library(vclib-external-eigen INTERFACE)
+    target_link_libraries(vclib-external-eigen INTERFACE Eigen3::Eigen)
 elseif(VCLIB_ALLOW_BUNDLED_EIGEN AND EXISTS "${VCLIB_EIGEN_DIR}/Eigen/Eigen")
     message(STATUS "- Eigen - using bundled source")
-    set(EIGEN_INCLUDE_DIRS ${VCLIB_EIGEN_DIR})
+    add_library(vclib-external-eigen INTERFACE)
+    target_include_directories(vclib-external-eigen INTERFACE ${VCLIB_EIGEN_DIR})
 else()
     message(
         FATAL_ERROR
         "Eigen is required - at least one of VCLIB_ALLOW_SYSTEM_EIGEN or VCLIB_ALLOW_BUNDLED_EIGEN must be enabled and found.")
 endif()
-
-add_library(vclib-external-eigen INTERFACE)
-
-target_include_directories(vclib-external-eigen INTERFACE ${EIGEN_INCLUDE_DIRS})
 
 list(APPEND VCLIB_EXTERNAL_LIBRARIES vclib-external-eigen)
