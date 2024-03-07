b43ccfa9032885f7e1867799bb663ca52651bfb2
Alessandro Muntoni <alemuntoni@users.noreply.github.com>
2024-03-07 14:29:40 +0100

    update examples with vclib

--- cmake/examples.cmake
+++ cmake/examples.cmake
@@ -20,26 +20,23 @@
 #* (https://www.mozilla.org/en-US/MPL/2.0/) for more details.                *
 #****************************************************************************/
 
-# todo: include_vclib_example_dir parameter should not be a bool, but an
-# optional parameter that represent the associated example name of the vclib
-# library (used to include relative example directory)
-
-function(vclib_render_add_example name include_vclib_example_dir)
-    cmake_parse_arguments(ARG "" "" "" ${ARGN})
+# declare a function vclib_render_add_example:
+# vclib_render_add_example(<example_name> SOURCES <source_files>... [VCLIB_EXAMPLE <vclib example>])
+# where:
+# example_name: the name of the example
+# source_files: the source files of the example
+# vclib_example: the name of the vclib example associated with the example
+function(vclib_render_add_example name)
+    cmake_parse_arguments(ARG "" "VCLIB_EXAMPLE" "SOURCES" ${ARGN})
 
     set(TARGET_NAME "vclib-render-example-${name}")
 
-    add_executable(${TARGET_NAME} ${ARG_UNPARSED_ARGUMENTS})
+    add_executable(${TARGET_NAME} ${ARG_SOURCES})
     target_link_libraries(${TARGET_NAME} PRIVATE
         vclib-render vclib-examples-common vclib-render-examples-common)
 
-    if (${include_vclib_example_dir}) # todo: change this, see above
-        string(REPLACE "-" "_" out_name ${name})
-        #remove first 4 chrs from out_name
-        string(SUBSTRING ${out_name} 4 -1 out_name)
-
-        set(VCLIB_INCLUDE_EXAMPLES_DIR ${VCLIB_EXAMPLES_DIR}/${out_name})
-
+    if (ARG_VCLIB_EXAMPLE)
+        set(VCLIB_INCLUDE_EXAMPLES_DIR ${VCLIB_EXAMPLES_DIR}/${ARG_VCLIB_EXAMPLE})
         target_include_directories(${TARGET_NAME} PUBLIC
                 ${VCLIB_INCLUDE_EXAMPLES_DIR})
     endif()
