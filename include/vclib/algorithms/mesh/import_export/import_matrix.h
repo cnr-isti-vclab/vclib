b14537446c2f695b88340941b48407c91bae3292
Alessandro Muntoni <alemuntoni@users.noreply.github.com>
2024-04-24 14:48:17 +0200

    bugfixes in import_matrix

--- include/vclib/algorithms/mesh/import_export/import_matrix.h
+++ include/vclib/algorithms/mesh/import_export/import_matrix.h
@@ -448,6 +448,8 @@ void importFacesFromMatrix(
 
             for (uint j = 0; j < vertexNumber; ++j)
                 f.setVertex(j, &mesh.vertex(faces(i, j)));
+
+            ++i;
         }
     }
     else { // the vertex number of mesh faces is fixed
@@ -455,11 +457,11 @@ void importFacesFromMatrix(
 
         constexpr int VN = FaceType::VERTEX_NUMBER;
         if (faces.cols() == VN) { // faces of matrix and mesh have same size
-
             uint i = 0;
             for (auto& f : mesh.faces()) {
                 for (uint j = 0; j < VN; ++j)
                     f.setVertex(j, &mesh.vertex(faces(i, j)));
+                ++i;
             }
         }
         else { // faces of matrix and mesh have different sizes
