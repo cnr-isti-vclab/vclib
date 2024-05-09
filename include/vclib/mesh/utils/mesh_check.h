6608eaee07e1492230909f0af4cd1aee79d598eb
Alessandro Muntoni <alemuntoni@users.noreply.github.com>
2024-05-09 17:34:53 +0200

    fix build issue on macos

--- include/vclib/mesh/utils/mesh_check.h
+++ include/vclib/mesh/utils/mesh_check.h
@@ -228,7 +228,7 @@ bool checkMeshPointers(const MeshType& mesh, TypeWrapper<Containers...>)
 {
     return (
         checkElementPointers<Containers::ElementType::ELEMENT_ID>(
-            mesh, typename MeshType::Containers()) &&
+            mesh, TypeWrapper<Containers...>()) &&
         ...);
 }
 
