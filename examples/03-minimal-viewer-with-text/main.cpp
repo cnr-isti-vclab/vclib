1e66f3906428f33e2d74b4d6dfaf04a9f6b46f3b
Alessandro Muntoni <alemuntoni@users.noreply.github.com>
2024-05-16 12:27:12 +0200

    text example

---
+++ examples/03-minimal-viewer-with-text/main.cpp
@@ -0,0 +1,64 @@
+/*****************************************************************************
+ * VCLib                                                                     *
+ * Visual Computing Library                                                  *
+ *                                                                           *
+ * Copyright(C) 2021-2024                                                    *
+ * Visual Computing Lab                                                      *
+ * ISTI - Italian National Research Council                                  *
+ *                                                                           *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This program is free software; you can redistribute it and/or modify      *
+ * it under the terms of the GNU General Public License as published by      *
+ * the Free Software Foundation; either version 3 of the License, or         *
+ * (at your option) any later version.                                       *
+ *                                                                           *
+ * This program is distributed in the hope that it will be useful,           *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              *
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
+ * for more details.                                                         *
+ ****************************************************************************/
+
+#include "common.h"
+
+#ifdef USE_QT
+#include <QApplication>
+#include <vclib/ext/qt/bgfx/minimal_viewer_widget.h>
+#elif USE_GLFW
+#include <vclib/ext/glfw/bgfx/minimal_viewer_window.h>
+#endif
+
+int main(int argc, char** argv)
+{
+#ifdef USE_QT
+    QApplication app(argc, argv);
+
+    vcl::qbgf::MinimalViewerWidget tw("Minimal Viewer Qt");
+#elif USE_GLFW
+    vcl::bglfwx::MinimalViewerWindow tw("Minimal Viewer GLFW");
+#endif
+    // load and set up a drawable mesh
+    vcl::TriMesh m = getMesh("greek_helmet.obj");
+    vcl::bgf::DrawableMesh<vcl::TriMesh> drawable = getDrawableMesh(m);
+
+    // add the drawable mesh to the scene
+    // the viewer will own **a copy** of the drawable mesh
+    tw.pushDrawableObject(drawable);
+
+    tw.enableText();
+
+    tw.setTextFont("DroidSans", 20);
+    tw.appendStaticText({5, 5}, "Vertices: " + std::to_string(m.vertexNumber()));
+    tw.appendStaticText({5, 30}, "Faces: " + std::to_string(m.faceNumber()));
+
+    tw.fitScene();
+
+    tw.show();
+
+#ifdef USE_QT
+    return app.exec();
+#elif USE_GLFW
+    return 0;
+#endif
+}
