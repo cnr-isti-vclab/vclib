9f62b9ce8f709e62ebc9d0db1d2ffa3ffc38e8a5
Alessandro Muntoni <alemuntoni@users.noreply.github.com>
2024-05-28 11:19:02 +0200

    mainwindow opengl2 example

---
+++ examples/02-main-window/opengl2/qt/common.h
@@ -0,0 +1,58 @@
+/*****************************************************************************
+ * VCLib                                                                     *
+ * Visual Computing Library                                                  *
+ *                                                                           *
+ * Copyright(C) 2021-2024                                                    *
+ * Visual Computing Lab                                                      *
+ * ISTI - Italian National Research Council                                  *
+ *                                                                           *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This program is free software; you can redistribute it and/or modify      *
+ * it under the terms of the GNU General Public License as published by      *
+ * the Free Software Foundation; either version 3 of the License, or         *
+ * (at your option) any later version.                                       *
+ *                                                                           *
+ * This program is distributed in the hope that it will be useful,           *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              *
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
+ * for more details.                                                         *
+ ****************************************************************************/
+
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <vclib/algorithms/mesh/update/color.h>
+#include <vclib/algorithms/mesh/update/normal.h>
+#include <vclib/load_save.h>
+#include <vclib/meshes/poly_mesh.h>
+
+#include <vclib/render/drawable/drawable_mesh.h>
+
+template<vcl::MeshConcept MeshType>
+vcl::DrawableMesh<MeshType> getDrawableMesh()
+{
+    // load a mesh:
+    MeshType m = vcl::load<MeshType>(VCLIB_ASSETS_PATH "/greek_helmet.obj");
+    vcl::updatePerVertexAndFaceNormals(m);
+
+    // enable the vertex color of the mesh and set it to gray
+    m.enablePerVertexColor();
+    vcl::setPerVertexColor(m, vcl::Color::Gray);
+
+    // create a MeshRenderSettings object, that allows to set the rendering
+    // options of the mesh
+    // default is what we want: color per vertex, smooth shading, no wireframe
+    vcl::MeshRenderSettings settings(m);
+
+    // create a DrawableMesh object from the mesh
+    vcl::DrawableMesh<MeshType> drawable(m);
+
+    // set the settings to the drawable mesh
+    drawable.setRenderSettings(settings);
+
+    return drawable;
+}
+
+#endif // COMMON_H
