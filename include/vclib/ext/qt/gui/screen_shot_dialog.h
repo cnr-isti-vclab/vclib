28666bccb76b899d9e67dd96ebf951961f9ec046
Alessandro Muntoni <alemuntoni@users.noreply.github.com>
2024-06-04 16:44:49 +0200

    small fixes

--- include/vclib/ext/qt/gui/screen_shot_dialog.h
+++ include/vclib/ext/qt/gui/screen_shot_dialog.h
@@ -1,5 +1,27 @@
-#ifndef SCREEN_SHOT_DIALOG_H
-#define SCREEN_SHOT_DIALOG_H
+/*****************************************************************************
+ * VCLib                                                                     *
+ * Visual Computing Library                                                  *
+ *                                                                           *
+ * Copyright(C) 2021-2024                                                    *
+ * Visual Computing Lab                                                      *
+ * ISTI - Italian National Research Council                                  *
+ *                                                                           *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This program is free software; you can redistribute it and/or modify      *
+ * it under the terms of the Mozilla Public License Version 2.0 as published *
+ * by the Mozilla Foundation; either version 2 of the License, or            *
+ * (at your option) any later version.                                       *
+ *                                                                           *
+ * This program is distributed in the hope that it will be useful,           *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              *
+ * Mozilla Public License Version 2.0                                        *
+ * (https://www.mozilla.org/en-US/MPL/2.0/) for more details.                *
+ ****************************************************************************/
+
+#ifndef VCL_EXT_QT_GUI_SCREEN_SHOT_DIALOG_H
+#define VCL_EXT_QT_GUI_SCREEN_SHOT_DIALOG_H
 
 #include <QFileDialog>
 
@@ -22,4 +44,4 @@ public:
 
 } // namespace vcl::qt
 
-#endif // SCREEN_SHOT_DIALOG_H
+#endif // VCL_EXT_QT_GUI_SCREEN_SHOT_DIALOG_H
