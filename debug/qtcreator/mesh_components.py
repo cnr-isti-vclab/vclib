1026ebabd1afb65fd6b8c57f3cb71784e31718ff
alemuntoni <alemuntoni@users.noreply.github.com>
2024-08-24 12:00:15 +0200

    [debug] qt debugging helpers for coordinate component

--- debug/qtcreator/mesh_components.py
+++ debug/qtcreator/mesh_components.py
@@ -1,4 +1,16 @@
 from dumper import *
 
+def is_vertical_component(value):
+    compValue = value.members(True)[0]
+    return compValue.type.templateArgument(4)
 
-
+def qdump__vcl__comp__Coordinate(d, value):
+    if is_vertical_component(value):
+        d.putValue('Vertical Component')
+    else:
+        d.putItem(value["mData"]["mData"])
+        # d.putExpandable()
+        # d.putNumChild(1)
+        # if d.isExpanded():
+        #     with Children(d):
+        #         d.putSubItem("Coordinate", value["mData"]["mData"])
