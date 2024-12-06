59afd68c920ba8ee4017ebdcc3156d7eec2f3d84
alemuntoni <alemuntoni@users.noreply.github.com>
2024-12-06 15:42:35 +0100

    [render] bgfx-qt misc example (static polymorphism)

---
+++ examples/render/999-misc/bgfx-qt/main.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+
+#include <QWidget>
+#include <QApplication>
+#include <QPushButton>
+#include <QVBoxLayout>
+
+#include <vclib/qt/viewer_widget.h>
+
+template<typename T>
+class Basic : public vcl::qt::ViewerWidget
+{
+    T t = 42;
+public:
+    Basic(QWidget* parent = nullptr) : vcl::qt::ViewerWidget(parent)
+    {
+        // add a QPushButton
+        QPushButton* button = new QPushButton("Click me", this);
+
+        // connect the button to a slot
+        connect(button, &QPushButton::clicked, this, &Basic::onButtonClicked);
+
+        // add the button to a layout
+        QVBoxLayout* layout = new QVBoxLayout(this);
+        layout->addWidget(button);
+        this->setLayout(layout);
+    }
+
+    void f() { std::cout << "C::f()" << t << std::endl; }
+
+public slots:
+    void onButtonClicked() { std::cout << "C::onButtonClicked()" << std::endl; }
+};
+
+
+
+template<template<typename> typename C, template<typename> typename E>
+class Minestrone : public C<Minestrone<C, E>>, public E<Minestrone<C, E>>
+{
+public:
+    using EventManagerType = E<Minestrone<C, E>>;
+
+    void foo()
+    {
+        C<Minestrone<C, E>>::drawWithInit();
+    };
+};
+
+
+template<class Derived>
+class Canvas
+{
+public:
+    void drawWithInit()
+    {
+        derived().init();
+        draw();
+    };
+    virtual void draw() { std::cerr << "draw\n"; };
+
+private:
+    auto& derived() { return *static_cast<Derived*>(this); };
+};
+
+template<class Derived>
+class EventManager
+{
+public:
+    void init() { std::cerr << "init\n"; };
+};
+
+
+class Gesu : public Minestrone<Canvas, EventManager>
+{
+    virtual void draw() override { std::cerr << "Gesu::draw\n"; }
+};
+
+
+int main(int argc, char** argv)
+{
+    QApplication app(argc, argv);
+
+    Gesu M;
+
+    M.foo();
+
+    // Basic<int> c;
+
+    // c.show();
+
+    return app.exec();
+}
