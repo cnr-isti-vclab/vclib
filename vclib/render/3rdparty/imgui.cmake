436eef4d74b8f9f0519f79cd10bb669f3e5888e4
alemuntoni <alemuntoni@users.noreply.github.com>
2024-12-11 16:42:43 +0100

    [render] add -fPIC flag for imgui and add LICENSE

--- vclib/render/3rdparty/imgui.cmake
+++ vclib/render/3rdparty/imgui.cmake
@@ -49,6 +49,9 @@ if (TARGET vclib-3rd-glfw AND TARGET vclib-3rd-bgfx)
     # this is the way to include the custom imconfig.h
     # target_compile_definitions(imgui PRIVATE IMGUI_USER_CONFIG="<vclib/render_bgfx/imgui/imconfig.h>")
 
+    # add -fPIC flag to imgui target
+    set_target_properties(imgui PROPERTIES POSITION_INDEPENDENT_CODE ON)
+
     add_library(vclib-3rd-imgui INTERFACE)
     target_link_libraries(vclib-3rd-imgui INTERFACE imgui)
 
