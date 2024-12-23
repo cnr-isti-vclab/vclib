83453aa3b4b3f19c81809765d338a5872b91c5e6
alemuntoni <alemuntoni@users.noreply.github.com>
2024-12-23 12:12:08 +0100

    [render] move viewer drawer opengl implementation to header

--- vclib/render/include/vclib/opengl2/drawers/viewer_drawer.h
+++ vclib/render/include/vclib/opengl2/drawers/viewer_drawer.h
@@ -27,6 +27,15 @@
 
 #include <memory>
 
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#else
+#ifdef _WIN32
+#include <windows.h>
+#endif
+#include <GL/gl.h>
+#endif
+
 namespace vcl {
 
 class ViewerDrawerOpenGL2 : public AbstractViewer
@@ -34,16 +43,39 @@ class ViewerDrawerOpenGL2 : public AbstractViewer
     bool mReadRequested = false;
 
 public:
-    ViewerDrawerOpenGL2(
-        uint  width     = 1024,
-        uint  height    = 768);
+    ViewerDrawerOpenGL2(uint width = 1024, uint height = 768) :
+            AbstractViewer(width, height)
+    {
+    }
 
     ViewerDrawerOpenGL2(
         const std::shared_ptr<DrawableObjectVector>& v,
-        uint                                         width     = 1024,
-        uint                                         height    = 768);
+        uint                                         width = 1024,
+        uint height = 768) : ViewerDrawerOpenGL2(width, height)
+    {
+        setDrawableObjectVector(v);
+    }
 
-    void onInit() override;
+    void onInit() override
+    {
+        glEnable(GL_LIGHTING);
+        glEnable(GL_LIGHT0);
+        glEnable(GL_DEPTH_TEST);
+        glEnable(GL_COLOR_MATERIAL);
+        glEnable(GL_NORMALIZE);
+
+        float lightAmbient[] = {.2f, .2f, .2f, 1.0f};
+        float lightDiffuse[] = {.5f, .5f, .5f, 1.0f};
+
+        glLightfv(GL_LIGHT0, GL_AMBIENT, lightAmbient);
+        glLightfv(GL_LIGHT0, GL_DIFFUSE, lightDiffuse);
+
+        if (mDrawList) {
+            for (auto& obj : *mDrawList) {
+                obj->init();
+            }
+        }
+    }
 
     void toggleAxisVisibility() override
     {
@@ -55,16 +87,75 @@ public:
         // todo
     }
 
-    void onDrawContent(uint) override;
+    void onDrawContent(uint) override
+    {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        auto         tmp = light().direction();
+        vcl::Point4f lPos(tmp.x(), tmp.y(), tmp.z(), 0.0f);
+
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glMultMatrixf(projectionMatrix().data());
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        glLightfv(GL_LIGHT0, GL_POSITION, lPos.data());
+        glMultMatrixf(viewMatrix().data());
+
+        for (auto& obj : *mDrawList)
+            obj->draw();
+    }
 
     // events
-    void onResize(unsigned int width, unsigned int height) override;
+    void onResize(unsigned int width, unsigned int height) override
+    {
+        DTB::resizeViewer(width, height);
+        // update(); TODO
+    }
 
-   // void onMouseDoubleClick(
-   //     MouseButton::Enum   button,
-   //     double              x,
-   //     double              y,
-   //     const KeyModifiers& modifiers) override;
+    // void onMouseDoubleClick(
+    //     MouseButton::Enum   button,
+    //     double              x,
+    //     double              y,
+    //     const KeyModifiers& modifiers) override
+    // {
+    //     // FIXME: code duplication for both OpenGL2 and BGFX
+    //     if (mReadRequested)
+    //         return;
+
+    //     // get point
+    //     const Point2d p(x, y);
+
+    //     // get the homogeneous NDC flag
+    //     const bool homogeneousNDC = true;
+
+    //     // create the callback
+    //     const auto    proj     = projectionMatrix();
+    //     const auto    view     = viewMatrix();
+    //     const Point4f vp       = {.0f, .0f, float(size().x()), float(size().y())};
+    //     auto          callback = [=, this](std::vector<float> data) {
+    //         mReadRequested = false;
+
+    //         assert(data.size() == 1);
+    //         const float depth = data[0];
+    //         // if the depth is 1.0, the point is not in the scene
+    //         if (depth == 1.0f) {
+    //             return;
+    //         }
+
+    //         // unproject the point
+    //         const Point3f p2d(p.x(), vp[3] - p.y(), depth);
+    //         auto          unproj = unproject(
+    //             p2d, Matrix44<ScalarType>(proj * view), vp, homogeneousNDC);
+
+    //         this->focus(unproj);
+    //         this->update();
+    //     };
+
+    //     mReadRequested = this->readDepth(Point2i(p.x(), p.y()), callback);
+    //     if (mReadRequested)
+    //         update();
+    // }
 
 };
 
