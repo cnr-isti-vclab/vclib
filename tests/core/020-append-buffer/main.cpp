0cf67bcab00822e52735878a55a1bb8b2c52b5dc
alemuntoni <alemuntoni@users.noreply.github.com>
2025-01-29 16:17:39 +0100

    [algorithms] test some  append/replace functions

--- tests/core/020-append-buffer/main.cpp
+++ tests/core/020-append-buffer/main.cpp
@@ -133,6 +133,69 @@ void testCoordsMatrix(
     }
 }
 
+template<typename MatrixType>
+void testTriangleMatrix(
+    const auto& tm,
+    const auto& vwm,
+    const auto& vtd,
+    const auto& ftr)
+{
+    using MeshType = vcl::RemoveRef<decltype(tm)>;
+    using ScalarType = MeshType::VertexType::CoordType::ScalarType;
+
+    vcl::uint nVerts = tm.vertexNumber() + vtd.size();
+    vcl::Array2<ScalarType> verts(nVerts, 3);
+    vcl::vertexCoordsToBuffer(tm, verts.data());
+    vcl::appendDuplicateVertexCoordsToBuffer(tm, vtd, verts.data());
+
+    auto stg = vcl::matrixStorageType<MatrixType>();
+
+    auto tris = vcl::faceIndicesMatrix<MatrixType>(tm);
+    vcl::replaceFaceIndicesByVertexDuplicationToBuffer(
+        tm, vtd, ftr, tris.data(), 3, stg);
+
+    REQUIRE(tris.rows() == tm.faceNumber());
+    REQUIRE(tris.cols() == 3);
+
+    for (vcl::uint i = 0; const auto& f : tm.faces()) {
+        for (vcl::uint j = 0; j < 3; ++j) {
+            // original coordinates should correspond to the duplicated ones
+            // that are stored in verts matrix and indexed by tris matrix
+            REQUIRE(f.vertex(j)->coord().x() == verts(tris(i, j), 0));
+            REQUIRE(f.vertex(j)->coord().y() == verts(tris(i, j), 1));
+            REQUIRE(f.vertex(j)->coord().z() == verts(tris(i, j), 2));
+        }
+        ++i;
+    }
+}
+
+template<typename VectorType>
+void testVertexSelectionVector(
+    const auto& tm,
+    const auto& vwm,
+    const auto& vtd,
+    const auto& ftr)
+{
+    vcl::uint nRows = tm.vertexNumber() + vtd.size();
+    VectorType sel(nRows);
+
+    vcl::vertexSelectionToBuffer(tm, sel.data());
+    vcl::appendDuplicateVertexSelectionToBuffer(tm, vtd, sel.data());
+
+    vcl::uint i = 0;
+    // first vertices corresponds
+    for (const auto& v : tm.vertices()) {
+        REQUIRE((bool) sel[i] == v.selected());
+        ++i;
+    }
+
+    // following vertices are duplicates (same as the vtd indices)
+    for (vcl::uint vd : vtd) {
+        REQUIRE(sel[i] == tm.vertex(vd).selected());
+        ++i;
+    }
+}
+
 using Meshes  = std::tuple<vcl::TriMesh, vcl::PolyMesh, vcl::EdgeMesh>;
 using Meshesf = std::tuple<vcl::TriMeshf, vcl::PolyMeshf, vcl::EdgeMeshf>;
 using MeshesIndexed =
@@ -187,6 +250,76 @@ TEMPLATE_TEST_CASE(
         SECTION("vcl::Array2")
         {
             testCoordsMatrix<vcl::Array2<ScalarType>>(
+                tm, vertWedgeMap, vertsToDuplicate, facesToReassign);
+        }
+    }
+
+    SECTION("Set vertex indices after duplication...")
+    {
+        SECTION("Eigen Row Major")
+        {
+            testTriangleMatrix<EigenRowMatrix<vcl::uint>>(
+                tm, vertWedgeMap, vertsToDuplicate, facesToReassign);
+        }
+        SECTION("Eigen 3 Row Major")
+        {
+            testTriangleMatrix<Eigen3RowMatrix<vcl::uint>>(
+                tm, vertWedgeMap, vertsToDuplicate, facesToReassign);
+        }
+        SECTION("Eigen Col Major")
+        {
+            testTriangleMatrix<EigenColMatrix<vcl::uint>>(
+                tm, vertWedgeMap, vertsToDuplicate, facesToReassign);
+        }
+        SECTION("Eigen 3 Col Major")
+        {
+            testTriangleMatrix<Eigen3ColMatrix<vcl::uint>>(
+                tm, vertWedgeMap, vertsToDuplicate, facesToReassign);
+        }
+        SECTION("vcl::Array2")
+        {
+            testTriangleMatrix<vcl::Array2<vcl::uint>>(
+                tm, vertWedgeMap, vertsToDuplicate, facesToReassign);
+        }
+    }
+
+    SECTION("Append duplicated vertex selection...")
+    {
+        randomSelection<vcl::ElemId::VERTEX>(tm);
+
+        SECTION("Eigen Vector<vcl::uint>")
+        {
+            testVertexSelectionVector<Eigen::VectorX<vcl::uint>>(
+                tm, vertWedgeMap, vertsToDuplicate, facesToReassign);
+        }
+
+        SECTION("Eigen Vector<bool>")
+        {
+            testVertexSelectionVector<Eigen::VectorX<bool>>(
+                tm, vertWedgeMap, vertsToDuplicate, facesToReassign);
+        }
+
+        SECTION("std vector<vcl::uint>")
+        {
+            testVertexSelectionVector<std::vector<vcl::uint>>(
+                tm, vertWedgeMap, vertsToDuplicate, facesToReassign);
+        }
+
+        SECTION("std vector<char>")
+        {
+            testVertexSelectionVector<std::vector<char>>(
+                tm, vertWedgeMap, vertsToDuplicate, facesToReassign);
+        }
+
+        SECTION("vcl::Vector<vcl::uint>")
+        {
+            testVertexSelectionVector<vcl::Vector<vcl::uint, -1>>(
+                tm, vertWedgeMap, vertsToDuplicate, facesToReassign);
+        }
+
+        SECTION("vcl::Vector<char>")
+        {
+            testVertexSelectionVector<vcl::Vector<char, -1>>(
                 tm, vertWedgeMap, vertsToDuplicate, facesToReassign);
         }
     }
