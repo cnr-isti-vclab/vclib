2b4c5a57a460bee4a103f98a38947073644bdc60
alemuntoni <alemuntoni@users.noreply.github.com>
2025-02-07 16:27:22 +0100

    [render] remove support for Qt5

--- vclib/render/3rdparty/qt.cmake
+++ vclib/render/3rdparty/qt.cmake
@@ -20,41 +20,20 @@
 #* (https://www.mozilla.org/en-US/MPL/2.0/) for more details.                *
 #****************************************************************************/
 
-find_package(Qt5 COMPONENTS Core Xml OpenGL Gui Widgets QUIET)
 find_package(Qt6 COMPONENTS Core Xml OpenGL Gui Widgets OpenGLWidgets QUIET)
 
-if (Qt6_FOUND OR Qt5_FOUND)
-    set(QT_FOUND TRUE)
-    if(Qt6_FOUND)
-        set(QT_VER 6)
-    else()
-        set(QT_VER 5)
-    endif()
-endif()
-
 if (VCLIB_ALLOW_SYSTEM_QT)
-    if (QT_FOUND)
-        message(STATUS "- Qt${QT_VER} - using system-provided library")
+    if (Qt6_FOUND)
+        message(STATUS "- Qt6 - using system-provided library")
 
         add_library(vclib-3rd-qt INTERFACE)
         target_compile_definitions(vclib-3rd-qt INTERFACE
             VCLIB_WITH_QT)
 
-        # prefer Qt6
-        if (Qt6_FOUND)
-            target_link_libraries(vclib-3rd-qt INTERFACE Qt6::Core Qt6::Widgets Qt6::Xml)
+        target_link_libraries(vclib-3rd-qt INTERFACE Qt6::Core Qt6::Widgets Qt6::Xml)
 
-            if (OpenGL_FOUND)
-                target_link_libraries(vclib-3rd-qt INTERFACE Qt6::OpenGL Qt6::OpenGLWidgets)
-            endif()
-        endif()
-
-        if (Qt5_FOUND AND NOT Qt6_FOUND)
-            target_link_libraries(vclib-3rd-qt INTERFACE Qt5::Core Qt5::Widgets Qt5::Xml)
-
-            if (OpenGL_FOUND)
-                target_link_libraries(vclib-3rd-qt INTERFACE Qt5::OpenGL)
-            endif()
+        if (OpenGL_FOUND)
+            target_link_libraries(vclib-3rd-qt INTERFACE Qt6::OpenGL Qt6::OpenGLWidgets)
         endif()
 
         list(APPEND VCLIB_RENDER_3RDPARTY_LIBRARIES vclib-3rd-qt)
