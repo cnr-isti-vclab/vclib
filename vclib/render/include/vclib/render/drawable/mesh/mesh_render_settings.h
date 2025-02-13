5deb45814000903f0e399045cb1cc5b74d835ab6
alemuntoni <alemuntoni@users.noreply.github.com>
2025-02-13 18:00:38 +0100

    [render] MeshRenderSettings is single header class

--- vclib/render/include/vclib/render/drawable/mesh/mesh_render_settings.h
+++ vclib/render/include/vclib/render/drawable/mesh/mesh_render_settings.h
@@ -236,7 +236,15 @@ public:
 
     float pointWidth() const { return mPointWidth; }
 
-    vcl::Color pointUserColor() const;
+    vcl::Color pointUserColor() const
+    {
+        vcl::Color c;
+        c.setRedF(mPointUserColor[0]);
+        c.setGreenF(mPointUserColor[1]);
+        c.setBlueF(mPointUserColor[2]);
+        c.setAlphaF(mPointUserColor[3]);
+        return c;
+    }
 
     const float* pointUserColorData() const { return mPointUserColor; }
 
@@ -251,7 +259,12 @@ public:
         return is<MRI::Primitive::SURFACE>(s);
     }
 
-    vcl::Color surfaceUserColor() const;
+    vcl::Color surfaceUserColor() const
+    {
+        vcl::Color c;
+        c.setAbgr(mSurfUserColor);
+        return c;
+    }
 
     const uint* surfaceUserColorData() const { return &mSurfUserColor; }
 
@@ -268,7 +281,15 @@ public:
 
     int wireframeWidth() const { return mWrfWidth; }
 
-    vcl::Color wireframeUserColor() const;
+    vcl::Color wireframeUserColor() const
+    {
+        vcl::Color c;
+        c.setRedF(mWrfUserColor[0]);
+        c.setGreenF(mWrfUserColor[1]);
+        c.setBlueF(mWrfUserColor[2]);
+        c.setAlphaF(mWrfUserColor[3]);
+        return c;
+    }
 
     const float* wireframeUserColorData() const { return mWrfUserColor; }
 
@@ -285,7 +306,12 @@ public:
 
     int edgesWidth() const { return mEdgesWidth; }
 
-    vcl::Color edgesUserColor() const;
+    vcl::Color edgesUserColor() const
+    {
+        vcl::Color c;
+        c.setAbgr(mEdgesUserColor);
+        return c;
+    }
 
     const uint* edgesUserColorData() const { return &mEdgesUserColor; }
 
@@ -299,7 +325,16 @@ public:
      * @param[in] b: the visibility status to set.
      * @return `true` if the visibility status has been set, `false` otherwise.
      */
-    bool setVisibility(bool b);
+    bool setVisibility(bool b)
+    {
+        if (canBeVisible()) {
+            mDrawMode.visible() = b;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
     /**
      * @brief Sets given the shading option of the given primitive.
@@ -371,11 +406,44 @@ public:
         return set<MRI::Primitive::POINTS>(p, b);
     }
 
-    bool setPointsWidth(float width);
+    bool setPointsWidth(float width)
+    {
+        if (canPoints(MRI::Points::VISIBLE)) {
+            mPointWidth = width;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
-    bool setPointsUserColor(float r, float g, float b, float a = 1);
+    bool setPointsUserColor(float r, float g, float b, float a = 1)
+    {
+        if (canPoints(MRI::Points::VISIBLE)) {
+            mPointUserColor[0] = r;
+            mPointUserColor[1] = g;
+            mPointUserColor[2] = b;
+            mPointUserColor[3] = a;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
-    bool setPointsUserColor(const vcl::Color& c);
+    bool setPointsUserColor(const vcl::Color& c)
+    {
+        if (canPoints(MRI::Points::VISIBLE)) {
+            mPointUserColor[0] = c.redF();
+            mPointUserColor[1] = c.greenF();
+            mPointUserColor[2] = c.blueF();
+            mPointUserColor[3] = c.alphaF();
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
     /**
      * @brief Sets the given shading option of the surface.
@@ -399,9 +467,32 @@ public:
         return set<MRI::Primitive::SURFACE>(s, b);
     }
 
-    bool setSurfaceUserColor(float r, float g, float b, float a = 1);
+    bool setSurfaceUserColor(float r, float g, float b, float a = 1)
+    {
+        if (canSurface(MRI::Surface::VISIBLE)) {
+            vcl::Color c;
+            c.setRedF(r);
+            c.setGreenF(g);
+            c.setBlueF(b);
+            c.setAlphaF(a);
+            mSurfUserColor = c.abgr();
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
-    bool setSurfaceUserColor(const vcl::Color& c);
+    bool setSurfaceUserColor(const vcl::Color& c)
+    {
+        if (canSurface(MRI::Surface::VISIBLE)) {
+            mSurfUserColor = c.abgr();
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
     /**
      * @brief Sets the given shading option of the wireframe.
@@ -425,11 +516,44 @@ public:
         return set<MRI::Primitive::WIREFRAME>(w, b);
     }
 
-    bool setWireframeUserColor(float r, float g, float b, float a = 1);
+    bool setWireframeUserColor(float r, float g, float b, float a = 1)
+    {
+        if (canWireframe(MRI::Wireframe::VISIBLE)) {
+            mWrfUserColor[0] = r;
+            mWrfUserColor[1] = g;
+            mWrfUserColor[2] = b;
+            mWrfUserColor[3] = a;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
-    bool setWireframeUserColor(const vcl::Color& c);
+    bool setWireframeUserColor(const vcl::Color& c)
+    {
+        if (canWireframe(MRI::Wireframe::VISIBLE)) {
+            mWrfUserColor[0] = c.redF();
+            mWrfUserColor[1] = c.greenF();
+            mWrfUserColor[2] = c.blueF();
+            mWrfUserColor[3] = c.alphaF();
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
-    bool setWireframeWidth(int width);
+    bool setWireframeWidth(int width)
+    {
+        if (canWireframe(MRI::Wireframe::VISIBLE)) {
+            mWrfWidth = width;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
     /**
      * @brief Sets the given shading option of the edges.
@@ -453,11 +577,43 @@ public:
         return set<MRI::Primitive::EDGES>(e, b);
     }
 
-    bool setEdgesUserColor(float r, float g, float b, float a = 1);
+    bool setEdgesUserColor(float r, float g, float b, float a = 1)
+    {
+        if (canEdges(MRI::Edges::VISIBLE)) {
+            vcl::Color c;
+            c.setRedF(r);
+            c.setGreenF(g);
+            c.setBlueF(b);
+            c.setAlphaF(a);
+            mEdgesUserColor = c.abgr();
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
-    bool setEdgesUserColor(const vcl::Color& c);
+    bool setEdgesUserColor(const vcl::Color& c)
+    {
+        if (canEdges(MRI::Edges::VISIBLE)) {
+            mEdgesUserColor = c.abgr();
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
-    bool setEdgesWidth(int width);
+    bool setEdgesWidth(int width)
+    {
+        if (canEdges(MRI::Edges::VISIBLE)) {
+            mEdgesWidth = width;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
     template<MeshConcept MeshType>
     void setRenderCapabilityFrom(const MeshType& m)
@@ -585,7 +741,19 @@ public:
         mDrawMode &= mCapability;
     }
 
-    void setDefaultSettingsFromCapability();
+    void setDefaultSettingsFromCapability()
+    {
+        mDrawMode.reset();
+
+        if (canBeVisible()) {
+            setVisibility(true);
+
+            setDefaultSurfaceSettingsFromCapability();
+            setDefaultWireframeSettingsFromCapability();
+            setDefaultPointSettingsFromCapability();
+            setDefaultEdgeSettingsFromCapability();
+        }
+    }
 
 private:
     template<MeshRenderInfo::Primitive PRIMITIVE, typename Enum>
@@ -615,13 +783,108 @@ private:
         setCapability<MRI::Primitive::EDGES>(e, b);
     }
 
-    void setDefaultPointSettingsFromCapability();
+    void setDefaultPointSettingsFromCapability()
+    {
+        using enum MRI::Points;
 
-    void setDefaultSurfaceSettingsFromCapability();
+        if (canPoints(VISIBLE)) {
+            if (!canSurface(MRI::Surface::VISIBLE))
+                setPoints(VISIBLE, true);
+            setPoints(SHADING_NONE);
+            if (canPoints(SHADING_VERT)) {
+                setPoints(SHADING_VERT);
+            }
+            if (canPoints(COLOR_VERTEX)) {
+                setPoints(COLOR_VERTEX);
+            }
+            else {
+                setPoints(COLOR_USER);
+            }
+        }
+    }
 
-    void setDefaultWireframeSettingsFromCapability();
+    void setDefaultSurfaceSettingsFromCapability()
+    {
+        using enum MRI::Surface;
 
-    void setDefaultEdgeSettingsFromCapability();
+        if (canSurface(VISIBLE)) {
+            setSurface(VISIBLE, true);
+            // shading
+            if (canSurface(SHADING_SMOOTH)) {
+                setSurface(SHADING_SMOOTH);
+            }
+            else if (canSurface(SHADING_FLAT)) {
+                setSurface(SHADING_FLAT);
+            }
+            else {
+                setSurface(SHADING_NONE);
+            }
+            // color
+            if (canSurface(COLOR_VERTEX)) {
+                setSurface(COLOR_VERTEX);
+            }
+            else if (canSurface(COLOR_FACE)) {
+                setSurface(COLOR_FACE);
+            }
+            else if (canSurface(COLOR_WEDGE_TEX)) {
+                setSurface(COLOR_WEDGE_TEX);
+            }
+            else if (canSurface(COLOR_VERTEX_TEX)) {
+                setSurface(COLOR_VERTEX_TEX);
+            }
+            else if (canSurface(COLOR_MESH)) {
+                setSurface(COLOR_MESH);
+            }
+            else {
+                setSurface(COLOR_USER);
+            }
+        }
+    }
+
+    void setDefaultWireframeSettingsFromCapability()
+    {
+        using enum MRI::Wireframe;
+
+        if (canWireframe(VISIBLE)) {
+            if (canWireframe(SHADING_VERT)) {
+                setWireframe(SHADING_VERT);
+            }
+            else {
+                setWireframe(SHADING_NONE);
+            }
+            // wireframe color (defaults to user defined)
+            setWireframe(COLOR_USER);
+        }
+    }
+
+    void setDefaultEdgeSettingsFromCapability()
+    {
+        using enum MRI::Edges;
+
+        if (canEdges(VISIBLE)) {
+            setEdges(VISIBLE, true);
+
+            if (canEdges(SHADING_SMOOTH)) {
+                setEdges(SHADING_SMOOTH);
+            }
+            else if (canEdges(SHADING_FLAT)) {
+                setEdges(SHADING_FLAT);
+            }
+            else {
+                setEdges(SHADING_NONE);
+            }
+
+            if (canEdges(COLOR_VERTEX)) {
+                setEdges(COLOR_VERTEX);
+            }
+            else if (canEdges(COLOR_EDGE)) {
+                setEdges(COLOR_EDGE);
+            }
+            else {
+                setEdges(COLOR_USER);
+            }
+        }
+    }
 };
 
 } // namespace vcl
