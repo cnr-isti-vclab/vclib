6ac78dfc7f429bd0e2e8eddb2e4963969982d1be
alemuntoni <alemuntoni@users.noreply.github.com>
2025-03-05 17:56:53 +0100

    [processing] split manager in different classes

--- vclib/processing/include/vclib/processing/manager/action_manager/id_mesh_manager.h
+++ vclib/processing/include/vclib/processing/manager/action_manager/id_mesh_manager.h
@@ -20,76 +20,47 @@
  * (https://www.mozilla.org/en-US/MPL/2.0/) for more details.                *
  ****************************************************************************/
 
-#ifndef VCL_PROCESSING_MANAGER_ACTION_MANAGER_MANAGER_H
-#define VCL_PROCESSING_MANAGER_ACTION_MANAGER_MANAGER_H
+#ifndef VCL_PROCESSING_MANAGER_ACTION_MANAGER_ID_MESH_MANAGER_H
+#define VCL_PROCESSING_MANAGER_ACTION_MANAGER_ID_MESH_MANAGER_H
 
-#include "id_mesh_manager.h"
-#include "io_mesh_manager.h"
-#include "io_image_manager.h"
+#include "id_action_container.h"
 
 #include <vclib/processing/engine/action_interfaces.h>
 
-namespace vcl::proc {
+namespace vcl::proc::detail {
 
-std::vector<std::shared_ptr<Action>> actionInstances();
+class IDMeshManager {
+    static const uint MESH_TYPE_NUMBER = toUnderlying(MeshTypeId::COUNT);
 
-namespace detail {
+    std::array<IDActionContainer, MESH_TYPE_NUMBER> mFilterActions;
 
-class Manager : public IDMeshManager, public IOMeshManager, public IOImageManager
-{
-public:
-    Manager() { addDefaultActions(); }
-
+protected:
     void add(const std::shared_ptr<Action>& action)
     {
-        using enum Action::Type;
-
-        uint mt;
-
-        std::shared_ptr<IOAction> ioImageAction;
-        std::shared_ptr<IOAction> ioMeshAction;
-
-        switch (action->type()) {
-        case IMAGE_IO_ACTION:
-            ioImageAction = std::dynamic_pointer_cast<IOAction>(action);
-            IOImageManager::add(ioImageAction);
-            break;
-        case MESH_IO_ACTION:
-            checkMeshAction(action);
-            ioMeshAction = std::dynamic_pointer_cast<IOAction>(action);
-            IOMeshManager::add(ioMeshAction);
-            break;
-        case FILTER_ACTION:
-            checkMeshAction(action);
-            IDMeshManager::add(action);
-            break;
-        default: throw std::runtime_error("Action type not supported");
-        }
+        uint mt = toUnderlying(action->meshType());
+        mFilterActions[mt].add(action);
     }
 
-    template<vcl::Range R>
-    requires vcl::RangeOf<R, std::shared_ptr<Action>>
-    void add(R&& actions)
+public:
+    // filter
+
+    std::shared_ptr<Action> filterAction(const std::string& name, MeshTypeId mt)
+        const
     {
-        for (const auto& action : actions) {
-            add(action);
-        }
+        return mFilterActions[toUnderlying(mt)].action(name);
     }
 
-    void addDefaultActions() { add(actionInstances()); }
-
-private:
-    void checkMeshAction(const std::shared_ptr<Action>& action)
+    template<typename MeshType>
+    std::shared_ptr<FilterActionT<MeshType>> filterAction(
+        const std::string& name)
     {
-        if (action->meshType() >= MeshTypeId::COUNT) {
-            throw std::runtime_error(
-                "The Action MeshType is not supported by the ActionManager.");
-        }
+        auto act =
+            mFilterActions[toUnderlying(meshTypeId<MeshType>())].action(name);
+
+        return std::dynamic_pointer_cast<FilterActionT<MeshType>>(act);
     }
 };
 
-} // namespace detail
+} // namespace vcl::proc::detail
 
-} // namespace vcl::proc
-
-#endif // VCL_PROCESSING_MANAGER_ACTION_MANAGER_MANAGER_H
+#endif // VCL_PROCESSING_MANAGER_ACTION_MANAGER_ID_MESH_MANAGER_H
