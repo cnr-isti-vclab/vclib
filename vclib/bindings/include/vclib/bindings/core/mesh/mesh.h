9921e6bbaa114ecddb95d8e5352f7600928878b5
alemuntoni <alemuntoni@users.noreply.github.com>
2025-03-18 15:40:15 +0100

    [bindings] core module for bindings

---
+++ vclib/bindings/include/vclib/bindings/core/mesh/mesh.h
@@ -0,0 +1,66 @@
+/*****************************************************************************
+ * VCLib                                                                     *
+ * Visual Computing Library                                                  *
+ *                                                                           *
+ * Copyright(C) 2021-2025                                                    *
+ * Visual Computing Lab                                                      *
+ * ISTI - Italian National Research Council                                  *
+ *                                                                           *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This program is free software; you can redistribute it and/or modify      *
+ * it under the terms of the Mozilla Public License Version 2.0 as published *
+ * by the Mozilla Foundation; either version 2 of the License, or            *
+ * (at your option) any later version.                                       *
+ *                                                                           *
+ * This program is distributed in the hope that it will be useful,           *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              *
+ * Mozilla Public License Version 2.0                                        *
+ * (https://www.mozilla.org/en-US/MPL/2.0/) for more details.                *
+ ****************************************************************************/
+
+#ifndef VCL_BINDINGS_CORE_MESH_MESH_H
+#define VCL_BINDINGS_CORE_MESH_MESH_H
+
+#include "containers.h"
+#include "elements.h"
+
+#include <vclib/bindings/utils.h>
+
+#include <vclib/concepts/mesh.h>
+
+#include <pybind11/pybind11.h>
+
+namespace vcl::bind {
+
+template<MeshConcept MeshType>
+void initMesh(pybind11::module& m, const std::string& name)
+{
+    namespace py = pybind11;
+
+    // Create the class
+    pybind11::class_<MeshType> c(m, name.c_str());
+
+    c.def(py::init<>());
+
+    defCopy(c);
+
+    initVertex(c);
+    initVertexContainer(c);
+
+    if constexpr (HasFaces<MeshType>) {
+        initFace(c);
+        initFaceContainer(c);
+    }
+    if constexpr (HasEdges<MeshType>) {
+        initEdge(c);
+        initEdgeContainer(c);
+    }
+
+    initComponents(c);
+}
+
+} // namespace vcl::bind
+
+#endif // VCL_BINDINGS_CORE_MESH_MESH_H
---
+++ vclib/bindings/src/vclib/bindings/core/space/core/image.cpp
@@ -0,0 +1,47 @@
+/*****************************************************************************
+ * VCLib                                                                     *
+ * Visual Computing Library                                                  *
+ *                                                                           *
+ * Copyright(C) 2021-2025                                                    *
+ * Visual Computing Lab                                                      *
+ * ISTI - Italian National Research Council                                  *
+ *                                                                           *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This program is free software; you can redistribute it and/or modify      *
+ * it under the terms of the Mozilla Public License Version 2.0 as published *
+ * by the Mozilla Foundation; either version 2 of the License, or            *
+ * (at your option) any later version.                                       *
+ *                                                                           *
+ * This program is distributed in the hope that it will be useful,           *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              *
+ * Mozilla Public License Version 2.0                                        *
+ * (https://www.mozilla.org/en-US/MPL/2.0/) for more details.                *
+ ****************************************************************************/
+
+#include <vclib/bindings/core/space/core/image.h>
+
+#include <vclib/space/core.h>
+
+namespace vcl::bind {
+
+void initImage(pybind11::module& m)
+{
+    namespace py = pybind11;
+
+    py::class_<Image> c(m, "Image" /*, py::buffer_protocol()*/);
+    c.def(py::init<>());
+    c.def(py::init<std::string>());
+
+    c.def("is_null", &Image::isNull);
+    c.def("height", &Image::height);
+    c.def("width", &Image::width);
+    c.def("size_in_bytes", &Image::sizeInBytes);
+    c.def("pixel", &Image::pixel);
+    c.def("load", &Image::load);
+    c.def("save", &Image::save);
+    c.def("mirror", &Image::mirror);
+}
+
+} // namespace vcl::bind
