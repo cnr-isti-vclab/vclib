03cca313071ecc75de99e6a6792758aac1ee3b17
Giacomo Fagiuoli <g.fagiuoli@studenti.unipi.it>
2025-05-26 18:02:45 +0200

    980-benchmark: 	- continued working on test_battery.py

--- examples/render/980-benchmark/glfw/test_battery.py
+++ examples/render/980-benchmark/glfw/test_battery.py
@@ -1,25 +1,58 @@
-
 #!/usr/bin/python3
+import os;
+import sys;
+import subprocess;
+
 def main():
-    import os;
-    import sys;
-    import subprocess;
     executable_name = None;
     if os.name == "nt":
         executable_name = "./980-benchmark.exe"
     else:
         executable_name = "./980-benchmark"
-    shadingTypesOptions = ["", "--flat"];
-    shadingSplittingOptions  = ["", "--split", "--uber-static-if"];
-    resolutions = [["1920", "1080"], ["1366", "768"], ["1536", "864"], ["1280", "720"]];
+    shadingTypesOptions = [
+        {"option": "", "name": "smooth"},
+        {"option": "--flat", "name": "flat"}
+    ];
+    shadingSplittingOptions  = [
+        {"option": "", "name": "uber"}, 
+        {"option": "--split", "name": "split"}, 
+        {"option": "--uber-static-if", "name": "uber_static_if"}
+    ];
+    resolutions = [
+        ["1920", "1080"], 
+        ["1366", "768"], 
+        ["1536", "864"], 
+        ["1280", "720"]
+    ];
     #TODO: add the meshes to be used
     meshes = [];
     for mesh in meshes:
+        if not os.path.exists(mesh):
+            print(f"{mesh} not found, skipping model...", file=sys.stderr);
         for shadingTypeOption in shadingTypesOptions:
             for shadingSplittingOption in shadingSplittingOptions:
                 for resolution in resolutions:
-                    subprocess.run([executable_name, shadingSplittingOption, shadingTypeOption, "--res", resolution[0], resolution[1], mesh]);
+                    resultPath = f"./results/{mesh.split('/').pop()}"
+                    create_all_in_path(resultPath);
+                    toRun = [
+                        executable_name, 
+                        shadingSplittingOption["option"], 
+                        shadingTypeOption["option"], 
+                        "--res", resolution[0], resolution[1], 
+                        "-o", f"{resultPath}/{shadingSplittingOption['name']}_{shadingTypeOption['name']}_{resolution[0]}_{resolution[1]}.csv",
+                        mesh
+                    ];
+                    subprocess.run([x for x in toRun if x!=""]);
     return;
+
+def create_all_in_path(path: str):
+    split_path = [x for x in path.split("/") if x!= ""];
+    total_path = "";
+    for partial in split_path:
+        total_path+=partial
+        if(not os.path.exists(total_path)):
+            os.mkdir(total_path);
+        total_path+="/";
 
 if __name__ == "__main__":
     main();
