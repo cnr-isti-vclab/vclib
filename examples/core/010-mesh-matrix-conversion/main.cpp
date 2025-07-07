b26ef7a6a5fc7104308059088bda7080c8fb11b4
alemuntoni <alemuntoni@users.noreply.github.com>
2025-07-07 13:43:33 +0200

    [examples] 010 mesh matrix conversion

---
+++ examples/core/010-mesh-matrix-conversion/main.cpp
@@ -0,0 +1,48 @@
+/*****************************************************************************
+ * VCLib                                                                     *
+ * Visual Computing Library                                                  *
+ *                                                                           *
+ * Copyright(C) 2021-2025                                                    *
+ * Visual Computing Lab                                                      *
+ * ISTI - Italian National Research Council                                  *
+ *                                                                           *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This program is free software; you can redistribute it and/or modify      *
+ * it under the terms of the Mozilla Public License Version 2.0 as published *
+ * by the Mozilla Foundation; either version 2 of the License, or            *
+ * (at your option) any later version.                                       *
+ *                                                                           *
+ * This program is distributed in the hope that it will be useful,           *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              *
+ * Mozilla Public License Version 2.0                                        *
+ * (https://www.mozilla.org/en-US/MPL/2.0/) for more details.                *
+ ****************************************************************************/
+
+#include "mesh_matrix_conversion.h"
+
+int main()
+{
+    auto [importedMesh, cubeMesh] = matrixConversionExample();
+
+    /****** Save the created meshes ******/
+
+    std::cout << "\n=== Saving Meshes ===" << std::endl;
+
+    try {
+        std::string resultsPath = VCLIB_RESULTS_PATH;
+
+        // save the imported mesh after processing
+        vcl::save(importedMesh, resultsPath + "/010_imported_mesh.ply");
+
+        vcl::save(cubeMesh, resultsPath + "/010_created_cube.ply");
+
+        std::cout << "\nAll files have been saved to: " << resultsPath << "\n";
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Error in saving: " << e.what() << "\n";
+    }
+
+    return 0;
+}
