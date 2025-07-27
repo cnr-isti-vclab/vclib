70295db1e61858b69305f40ae0a953f29b2f1097
alemuntoni <alemuntoni@users.noreply.github.com>
2025-07-27 14:22:14 +0200

    [tests] add some static asserts for VertexRangeConcept

--- tests/core/000-static-asserts/meshes/mesh_views.h
+++ tests/core/000-static-asserts/meshes/mesh_views.h
@@ -75,6 +75,31 @@ void meshViewsStaticAsserts()
             std::ranges::range<decltype(cm | vcl::views::faces)>,
             "The view returned by pipe operation cm | views::faces is not a "
             "valid range.");
+
+        auto& f = m.face(0);
+        const auto& cf  = cm.face(0);
+
+        static_assert(
+            vcl::VertexRangeConcept<decltype(f.vertices() | vcl::views::deref)>,
+            "The view returned by f.vertices() is not a "
+            "VertexRangeConcept.");
+
+        static_assert(
+            vcl::VertexRangeConcept<
+                decltype(cf.vertices() | vcl::views::deref)>,
+            "The view returned by cf.vertices() is not a "
+            "VertexRangeConcept.");
+
+        // TODO: fix these
+        // static_assert(
+        //     vcl::VertexPointerRangeConcept<decltype(f.vertices())>,
+        //     "The view returned by f.vertices() is not a "
+        //     "VertexPointerRangeConcept.");
+
+        // static_assert(
+        //     vcl::VertexPointerRangeConcept<decltype(cf.vertices())>,
+        //     "The view returned by cf.vertices() is not a "
+        //     "VertexPointerRangeConcept.");
     }
 
     if constexpr (vcl::HasEdges<MeshType>) {
