eb13925b4866c56820662dcabc7c235457fbf807
Luigi Malomo <malomo@users.noreply.github.com>
2025-09-26 14:58:14 +0200

    [render] Add edge size slider to mesh viewer interface in imgui

--- vclib/render/include/vclib/imgui/mesh_viewer_imgui_drawer.h
+++ vclib/render/include/vclib/imgui/mesh_viewer_imgui_drawer.h
@@ -690,6 +690,22 @@ private:
             ImGuiColorEditFlags_NoInputs);
         ImGui::EndDisabled();
 
+        // edge size
+        ImGui::Text("Size:");
+        // set the width of the window minus the width of the label
+        ImGui::SameLine();
+        ImGui::SetNextItemWidth(-10);
+        ImGui::SliderFloat(
+            "##EdgeSize",
+            [&] {
+                return settings.edgesWidth();
+            },
+            [&](float v) {
+                settings.setEdgesWidth(v);
+            },
+            1.0f,
+            32.0f);
+
         ImGui::EndDisabled();
     }
 
