7e34c967ce1c3a674ebd07445b714c246a47d2f1
alemuntoni <alemuntoni@users.noreply.github.com>
2025-10-14 17:47:11 +0200

    [base] fix mesh function call on sort.h

--- vclib/core/include/vclib/algorithms/mesh/sort.h
+++ vclib/core/include/vclib/algorithms/mesh/sort.h
@@ -128,7 +128,7 @@ std::vector<uint> sortElemIndicesByFunction(
     std::vector<uint> indices;
 
     // Initialize indices with sequential values
-    indices.resize(mesh.template elementNumber<ELEM_ID>());
+    indices.resize(mesh.template number<ELEM_ID>());
     std::iota(indices.begin(), indices.end(), 0u);
 
     std::sort(
