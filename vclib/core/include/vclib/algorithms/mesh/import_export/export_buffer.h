72eeadfd83e745bdd928c4f7ea6d26572af43242
alemuntoni <alemuntoni@users.noreply.github.com>
2025-10-14 09:42:05 +0200

    [algorithms] feat: add utility functions for compact index retrieval in mesh algorithms

--- vclib/core/include/vclib/algorithms/mesh/import_export/export_buffer.h
+++ vclib/core/include/vclib/algorithms/mesh/import_export/export_buffer.h
@@ -25,6 +25,8 @@
 
 #include "detail.h"
 
+#include <vclib/algorithms/mesh/utility.h>
+
 #include <vclib/algorithms/core.h>
 #include <vclib/mesh.h>
 #include <vclib/space/complex.h>
@@ -42,42 +44,6 @@
 namespace vcl {
 
 namespace detail {
-
-// returns a non-empty vector if the ELEM_ID container is not compact and the
-// user wants compact indices
-template<uint ELEM_ID>
-std::vector<uint> vertCompactIndices(const auto& mesh, bool wantCompact)
-{
-    std::vector<uint> elemCompIndices;
-
-    bool isCompact = mesh.template number<ELEM_ID>() ==
-                     mesh.template containerSize<ELEM_ID>();
-
-    if (wantCompact && !isCompact)
-        elemCompIndices = mesh.template compactIndices<ELEM_ID>();
-    return elemCompIndices;
-}
-
-// returns a non-empty vector if the vertex container is not compact and the
-// user wants compact indices
-std::vector<uint> vertCompactIndices(const auto& mesh, bool wantCompact)
-{
-    return vertCompactIndices<ElemId::VERTEX>(mesh, wantCompact);
-}
-
-// lambda to get the vertex index of a face (considering compact vertex indices)
-auto vIndexLambda(const auto& mesh, const std::vector<uint>& vertCompIndices)
-{
-    // lambda to get the vertex index of a face (considering compact indices)
-    auto vIndex = [&vertCompIndices](const auto& f, uint i) {
-        if (vertCompIndices.size() > 0)
-            return vertCompIndices[f.vertexIndex(i)];
-        else
-            return f.vertexIndex(i);
-    };
-
-    return vIndex;
-}
 
 inline TriPolyIndexBiMap indexMap;
 
