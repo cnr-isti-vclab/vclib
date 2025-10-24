f0d08607a5055ab4f7c980d11f654e100f57fcd7
Davide Lupo <d.lupo2@studenti.unipi.it>
2025-10-24 11:45:02 +0200

    [assets] specified pbr examples to test on every platform

--- examples/render/950-pbr/main.cpp
+++ examples/render/950-pbr/main.cpp
@@ -38,14 +38,39 @@ int main(int argc, char** argv)
     std::string e6 = VCLIB_EXAMPLE_MESHES_PATH "/gltf/Sphere/opaque_grid.glb";
     std::string e7 = VCLIB_EXAMPLE_MESHES_PATH "/gltf/Sphere/mask_grid.glb";
     std::string e8 = VCLIB_EXAMPLE_MESHES_PATH "/gltf/Sphere/blend_grid.glb";
-    std::string e9 = VCLIB_EXAMPLE_MESHES_PATH "/gltf/EmissiveTest/emissive_grid.glb";
-    std::string e10 = VCLIB_EXAMPLE_MESHES_PATH "/gltf/VertexColorTest/vertex_color_grid.glb";
-    std::string e11 = VCLIB_EXAMPLE_MESHES_PATH "/gltf/DoubleSidedTest/OrientationTest.gltf";
-    std::string e12 = VCLIB_EXAMPLE_MESHES_PATH "/gltf/AlphaModeTest/opaque_grid.glb";
-    std::string e13 = VCLIB_EXAMPLE_MESHES_PATH "/gltf/AlphaModeTest/mask_grid.glb";
-    std::string e14 = VCLIB_EXAMPLE_MESHES_PATH "/gltf/AlphaModeTest/blend_grid.glb";    
 
-    std::vector<MaterialTriMesh> meshes = vcl::loadMeshes<MaterialTriMesh>(e14);
+    // Examples to test
+
+    // Metallic-Roughness
+    std::string t1 = VCLIB_EXAMPLE_MESHES_PATH "/gltf/MetalRoughSpheresNoTextures/MetalRoughSpheresNoTextures.gltf";
+
+    // Emissive - to test also with lights off comment the #define USE_LIGHTS in vclib/render/shaders/vclib/bgfx/shaders_common.sh
+    std::string t2 = VCLIB_EXAMPLE_MESHES_PATH "/gltf/EmissiveTest/emissive_grid.glb";
+
+    // Vertex Color - vertex color has the green channel growing on the y axis, 
+    // base color has the blue channel growing on the x axis
+    std::string t3 = VCLIB_EXAMPLE_MESHES_PATH "/gltf/VertexColorTest/vertex_color_grid.glb";
+
+    // Double Sided - modified version of OrientationTest with double sided enabled on the cube material, not the arrows'
+    std::string t4 = VCLIB_EXAMPLE_MESHES_PATH "/gltf/DoubleSidedTest/OrientationTest.gltf";
+
+    // Alpha Mode - opaque
+    std::string t5 = VCLIB_EXAMPLE_MESHES_PATH "/gltf/AlphaModeTest/opaque_grid.glb";
+
+    // Alpha Mode - mask, the alpha channel in the base color grows on the x axis
+    // while it's random between 0.5 and 1 in the vertex color
+    // the cutoff is set to 0.3 in the material hence the first 3 of the 10 columns should be basically invisible
+    // while the others are progressively more visible because of the increasing alpha in the base color
+    std::string t6 = VCLIB_EXAMPLE_MESHES_PATH "/gltf/AlphaModeTest/mask_grid.glb";
+
+    // Alpha Mode - blend, generated with the same criteria as t6
+    // the only change is the alpha mode set to BLEND
+    std::string t7 = VCLIB_EXAMPLE_MESHES_PATH "/gltf/AlphaModeTest/blend_grid.glb"; 
+
+    // Similar to the previous one, but with varying metallic and roughness
+    std::string t8 = VCLIB_EXAMPLE_MESHES_PATH "/gltf/Sphere/blend_grid.glb"; 
+
+    std::vector<MaterialTriMesh> meshes = vcl::loadMeshes<MaterialTriMesh>(t1);
 
     for(auto& mesh : meshes) {
         for (auto& mat : mesh.materials()) {
