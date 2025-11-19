16bdb36bc388816392df11f9d6aa73f18a951556
alemuntoni <alemuntoni@users.noreply.github.com>
2025-11-19 09:49:40 +0100

    [render] Add backend-specific DrawObjectSettings types for bgfx and opengl2

--- vclib/render/include/vclib/bgfx/drawable/draw_object_settings_bgfx.h
+++ vclib/render/include/vclib/bgfx/drawable/draw_object_settings_bgfx.h
@@ -20,27 +20,26 @@
  * (https://www.mozilla.org/en-US/MPL/2.0/) for more details.                *
  ****************************************************************************/
 
-#ifndef VCL_RENDER_DRAWABLE_DRAW_OBJECT_SETTINGS_H
-#define VCL_RENDER_DRAWABLE_DRAW_OBJECT_SETTINGS_H
+#ifndef VCL_BGFX_DRAWABLE_DRAW_OBJECT_SETTINGS_BGFX_H
+#define VCL_BGFX_DRAWABLE_DRAW_OBJECT_SETTINGS_BGFX_H
 
-#ifdef VCLIB_RENDER_BACKEND_BGFX
-#include <vclib/bgfx/drawable/draw_object_settings_bgfx.h>
-#endif
-
-#ifdef VCLIB_RENDER_BACKEND_OPENGL2
-#include <vclib/opengl2/drawable/draw_object_settings_opengl2.h>
-#endif
+#include <vclib/base.h>
 
 namespace vcl {
 
-#ifdef VCLIB_RENDER_BACKEND_BGFX
-using DrawObjectSettings = DrawObjectSettingsBGFX;
-#endif
+/**
+ * @brief A simple struct containing the settings to draw a drawable object
+ * in a bgfx canvas.
+ */
+struct DrawObjectSettingsBGFX
+{
+    /**< @brief The object ID to assign to the object. */
+    uint objectId = 0;
 
-#ifdef VCLIB_RENDER_BACKEND_OPENGL2
-using DrawObjectSettings = DrawObjectSettingsOpenGL2;
-#endif
+    /**< @brief The view ID on which to draw the object. */
+    uint viewId = 0;
+};
 
 } // namespace vcl
 
-#endif // VCL_RENDER_DRAWABLE_DRAW_OBJECT_SETTINGS_H
+#endif // VCL_BGFX_DRAWABLE_DRAW_OBJECT_SETTINGS_BGFX_H
