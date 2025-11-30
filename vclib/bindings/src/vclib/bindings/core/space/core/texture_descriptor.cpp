a02e334f0d90a87f315b76e755a88c439f28ee11
alemuntoni <alemuntoni@users.noreply.github.com>
2025-11-30 12:11:44 +0100

    [bindings] Expose enums and keyword arguments for Image, Material and TextureDescriptor bindings

--- vclib/bindings/src/vclib/bindings/core/space/core/texture_descriptor.cpp
+++ vclib/bindings/src/vclib/bindings/core/space/core/texture_descriptor.cpp
@@ -31,8 +31,39 @@ namespace vcl::bind {
 void initTextureDescriptor(pybind11::module& m)
 {
     namespace py = pybind11;
+    using namespace py::literals;
 
     py::class_<TextureDescriptor> c(m, "TextureDescriptor");
+
+    {
+        using enum TextureDescriptor::MinificationFilter;
+        py::enum_<TextureDescriptor::MinificationFilter> mif(
+            c, "MinificationFilter");
+        mif.value("NONE", NONE);
+        mif.value("NEAREST", NEAREST);
+        mif.value("LINEAR", LINEAR);
+        mif.value("NEAREST_MIPMAP_NEAREST", NEAREST_MIPMAP_NEAREST);
+        mif.value("LINEAR_MIPMAP_NEAREST", LINEAR_MIPMAP_NEAREST);
+        mif.value("NEAREST_MIPMAP_LINEAR", NEAREST_MIPMAP_LINEAR);
+        mif.value("LINEAR_MIPMAP_LINEAR", LINEAR_MIPMAP_LINEAR);
+        mif.export_values();
+    }
+
+    using enum TextureDescriptor::MagnificationFilter;
+    py::enum_<TextureDescriptor::MagnificationFilter> magf(
+        c, "MagnificationFilter");
+    magf.value("NONE", NONE);
+    magf.value("NEAREST", NEAREST);
+    magf.value("LINEAR", LINEAR);
+    magf.export_values();
+
+    using enum TextureDescriptor::WrapMode;
+    py::enum_<TextureDescriptor::WrapMode> wm(c, "WrapMode");
+    wm.value("REPEAT", REPEAT);
+    wm.value("CLAMP_TO_EDGE", CLAMP_TO_EDGE);
+    wm.value("MIRRORED_REPEAT", MIRRORED_REPEAT);
+    wm.export_values();
+
     c.def(py::init<>());
 
     c.def("is_null", &TextureDescriptor::isNull);
@@ -40,7 +71,44 @@ void initTextureDescriptor(pybind11::module& m)
     c.def("path", py::overload_cast<>(&TextureDescriptor::path, py::const_));
     c.def("set_path", [](TextureDescriptor& t, const std::string& p) {
         t.path() = p;
-    });
+    },
+          "path"_a);
+    c.def(
+        "min_filter",
+        py::overload_cast<>(&TextureDescriptor::minFilter, py::const_));
+    c.def(
+        "set_min_filter",
+        [](TextureDescriptor& t, TextureDescriptor::MinificationFilter mf) {
+            t.minFilter() = mf;
+        },
+        "min_filter"_a);
+    c.def(
+        "mag_filter",
+        py::overload_cast<>(&TextureDescriptor::magFilter, py::const_));
+    c.def(
+        "set_mag_filter",
+        [](TextureDescriptor& t, TextureDescriptor::MagnificationFilter mf) {
+            t.magFilter() = mf;
+        },
+        "mag_filter"_a);
+    c.def(
+        "wrap_u",
+        py::overload_cast<>(&TextureDescriptor::wrapU, py::const_));
+    c.def(
+        "set_wrap_u",
+        [](TextureDescriptor& t, TextureDescriptor::WrapMode wm) {
+            t.wrapU() = wm;
+        },
+        "wrap_u"_a);
+    c.def(
+        "wrap_v",
+        py::overload_cast<>(&TextureDescriptor::wrapV, py::const_));
+    c.def(
+        "set_wrap_v",
+        [](TextureDescriptor& t, TextureDescriptor::WrapMode wm) {
+            t.wrapV() = wm;
+        },
+        "wrap_v"_a);
 }
 
 } // namespace vcl::bind
