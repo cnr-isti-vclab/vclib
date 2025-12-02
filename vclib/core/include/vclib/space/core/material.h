a0f9462db4afb36b48096c856b23443341dfe1a2
alemuntoni <alemuntoni@users.noreply.github.com>
2025-12-02 11:07:23 +0100

    [space] Add detailed documentation comments to Material and TextureDescriptor classes

--- vclib/core/include/vclib/space/core/material.h
+++ vclib/core/include/vclib/space/core/material.h
@@ -32,20 +32,43 @@
 namespace vcl {
 
 /**
- * @brief Material class represents a material with properties needed for
- * rendering (PBR).
+ * @brief Represents a Physically-Based Rendering (PBR) material.
+ *
+ * This class encapsulates all the properties required to describe a surface
+ * for rendering, following the PBR metallic-roughness workflow. It includes
+ * base color, metallic and roughness factors, emissive color, and textures.
+ * It also handles transparency modes and other rendering-related attributes.
+ *
+ * @ingroup space_core
  */
 class Material
 {
 public:
-    enum class AlphaMode { ALPHA_OPAQUE, ALPHA_MASK, ALPHA_BLEND };
+    /**
+     * @brief Defines the alpha rendering mode of the material.
+     */
+    enum class AlphaMode {
+        ALPHA_OPAQUE, ///< The material is fully opaque. Alpha values are
+                      ///< ignored.
+        ALPHA_MASK,   ///< The material is rendered as either fully opaque or
+                      ///< fully transparent, based on the alphaCutoff value.
+        ALPHA_BLEND ///< The material is rendered with alpha blending, allowing
+                    ///< for smooth transparency.
+    };
+
+    /**
+     * @brief Defines the types of textures used in the PBR material model.
+     */
     enum class TextureType {
-        BASE_COLOR = 0,
-        METALLIC_ROUGHNESS,
-        NORMAL,
-        OCCLUSION,
-        EMISSIVE,
-        COUNT
+        BASE_COLOR =
+            0, ///< The base color (albedo) texture. Stored in sRGB color space.
+        METALLIC_ROUGHNESS, ///< Metallic (B channel) and roughness (G channel)
+                            ///< texture. Stored in linear color space.
+        NORMAL, ///< The tangent-space normal map. Stored in linear color space.
+        OCCLUSION, ///< The ambient occlusion map (R channel). Stored in linear
+                   ///< color space.
+        EMISSIVE,  ///< The emissive color texture. Stored in sRGB color space.
+        COUNT      ///< Utility value to get the number of texture types.
     };
 
 private:
@@ -57,7 +80,7 @@ private:
     // essential PBR properties
     Color mBaseColor = Color::White;
 
-    float mMetallic  = 1.0f;
+    float mMetallic = 1.0f;
 
     float mRoughness = 1.0f;
 
@@ -73,86 +96,214 @@ private:
     float mOcclusionStrength = 1.0f;
 
     std::array<TextureDescriptor, N_TEXTURE_TYPE> mTextureDescriptors;
-    
+
     bool mDoubleSided = false;
-    
+
 public:
+    /**
+     * @brief Default constructor.
+     *
+     * Creates a default material.
+     * TODO: define the properties of the default material.
+     */
     Material() {}
 
+    /**
+     * @brief Gets the name of the material.
+     * @return A const reference to the material's name.
+     */
     const std::string& name() const { return mName; }
 
+    /**
+     * @brief Gets a mutable reference to the name of the material.
+     * @return A reference to the material's name.
+     */
     std::string& name() { return mName; }
 
+    /**
+     * @brief Gets the base color of the material.
+     * This is used as a tint if a base color texture is present.
+     * @return A const reference to the base color.
+     */
     const Color& baseColor() const { return mBaseColor; }
 
+    /**
+     * @brief Gets a mutable reference to the base color of the material.
+     * @return A reference to the base color.
+     */
     Color& baseColor() { return mBaseColor; }
 
+    /**
+     * @brief Gets the metallic factor of the material.
+     * @return The metallic factor, in the range [0.0, 1.0].
+     */
     float metallic() const { return mMetallic; }
 
+    /**
+     * @brief Gets a mutable reference to the metallic factor of the material.
+     * @return A reference to the metallic factor.
+     */
     float& metallic() { return mMetallic; }
 
+    /**
+     * @brief Gets the roughness factor of the material.
+     * @return The roughness factor, in the range [0.0, 1.0].
+     */
     float roughness() const { return mRoughness; }
 
+    /**
+     * @brief Gets a mutable reference to the roughness factor of the material.
+     * @return A reference to the roughness factor.
+     */
     float& roughness() { return mRoughness; }
 
+    /**
+     * @brief Gets the emissive color of the material.
+     * This is the color emitted by the material, independent of lighting.
+     * @return A const reference to the emissive color.
+     */
     const Color& emissiveColor() const { return mEmissiveColor; }
 
+    /**
+     * @brief Gets a mutable reference to the emissive color of the material.
+     * @return A reference to the emissive color.
+     */
     Color& emissiveColor() { return mEmissiveColor; }
 
+    /**
+     * @brief Gets the alpha rendering mode.
+     * @return The current alpha mode (Opaque, Mask, or Blend).
+     */
     AlphaMode alphaMode() const { return mAlphaMode; }
 
+    /**
+     * @brief Gets a mutable reference to the alpha rendering mode.
+     * @return A reference to the alpha mode.
+     */
     AlphaMode& alphaMode() { return mAlphaMode; }
 
+    /**
+     * @brief Gets the alpha cutoff value.
+     * This value is used only when the alpha mode is `ALPHA_MASK`.
+     * @return The alpha cutoff value, in the range [0.0, 1.0].
+     */
     float alphaCutoff() const { return mAlphaCutoff; }
 
+    /**
+     * @brief Gets a mutable reference to the alpha cutoff value.
+     * @return A reference to the alpha cutoff value.
+     */
     float& alphaCutoff() { return mAlphaCutoff; }
 
+    /**
+     * @brief Checks if the material is double-sided.
+     * If true, both front and back faces of polygons with this material should
+     * be rendered.
+     * @return True if the material is double-sided, false otherwise.
+     */
     bool doubleSided() const { return mDoubleSided; }
 
+    /**
+     * @brief Gets a mutable reference to the double-sided property.
+     * @return A reference to the double-sided flag.
+     */
     bool& doubleSided() { return mDoubleSided; }
 
+    /**
+     * @brief Gets the scalar multiplier for the normal map.
+     * @return The normal scale factor.
+     */
     float normalScale() const { return mNormalScale; }
 
+    /**
+     * @brief Gets a mutable reference to the normal scale.
+     * @return A reference to the normal scale.
+     */
     float& normalScale() { return mNormalScale; }
 
+    /**
+     * @brief Gets the strength of the ambient occlusion effect.
+     * @return The occlusion strength, in the range [0.0, 1.0].
+     */
     float occlusionStrength() const { return mOcclusionStrength; }
 
+    /**
+     * @brief Gets a mutable reference to the occlusion strength.
+     * @return A reference to the occlusion strength.
+     */
     float& occlusionStrength() { return mOcclusionStrength; }
 
+    /**
+     * @brief Gets the texture descriptor for the base color texture.
+     * @return A const reference to the base color texture descriptor.
+     */
     const TextureDescriptor& baseColorTextureDescriptor() const
     {
         using enum TextureType;
         return mTextureDescriptors[toUnderlying(BASE_COLOR)];
     }
 
+    /**
+     * @brief Gets a mutable reference to the texture descriptor for the base
+     * color texture.
+     * @return A reference to the base color texture descriptor.
+     */
     TextureDescriptor& baseColorTextureDescriptor()
     {
         using enum TextureType;
         return mTextureDescriptors[toUnderlying(BASE_COLOR)];
     }
 
+    /**
+     * @brief Gets the texture descriptor for a given texture type index.
+     * @param[in] type: The integer index of the texture type. Must be less than
+     * `TextureType::COUNT`.
+     * @return A const reference to the corresponding texture descriptor.
+     */
     const TextureDescriptor& textureDescriptor(uint type) const
     {
         assert(type < N_TEXTURE_TYPE);
         return mTextureDescriptors[type];
     }
 
+    /**
+     * @brief Gets the texture descriptor for a given texture type.
+     * @param[in] type: The TextureType enum value.
+     * @return A const reference to the corresponding texture descriptor.
+     */
     const TextureDescriptor& textureDescriptor(TextureType type) const
     {
         return textureDescriptor(toUnderlying(type));
     }
 
+    /**
+     * @brief Gets a mutable reference to the texture descriptor for a given
+     * texture type index.
+     * @param[in] type: The integer index of the texture type. Must be less than
+     * `TextureType::COUNT`.
+     * @return A reference to the corresponding texture descriptor.
+     */
     TextureDescriptor& textureDescriptor(uint type)
     {
         assert(type < N_TEXTURE_TYPE);
         return mTextureDescriptors[type];
     }
 
+    /**
+     * @brief Gets a mutable reference to the texture descriptor for a given
+     * texture type.
+     * @param[in] type: The TextureType enum value.
+     * @return A reference to the corresponding texture descriptor.
+     */
     TextureDescriptor& textureDescriptor(TextureType type)
     {
         return textureDescriptor(toUnderlying(type));
     }
 
+    /**
+     * @brief Serializes the material's data to an output stream.
+     * @param[in/out] os: The output stream to write to.
+     */
     void serialize(std::ostream& os) const
     {
         vcl::serialize(os, mName);
@@ -166,6 +317,10 @@ public:
         vcl::serialize(os, mDoubleSided);
     }
 
+    /**
+     * @brief Deserializes the material's data from an input stream.
+     * @param[in/out] is: The input stream to read from.
+     */
     void deserialize(std::istream& is)
     {
         vcl::deserialize(is, mName);
@@ -179,19 +334,33 @@ public:
         vcl::deserialize(is, mDoubleSided);
     }
 
+    /**
+     * @brief Defaulted equality comparison operator.
+     * @param[in] other: The material to compare against.
+     * @return True if all members of this material are equal to the other,
+     * false otherwise.
+     */
     bool operator==(const Material& other) const = default;
 
+    /**
+     * @brief Determines the appropriate color space for a given texture type.
+     *
+     * Base color and emissive textures contain color information and should be
+     * treated as sRGB. Other textures (metallic-roughness, normal, occlusion)
+     * contain data and should be treated as linear.
+     *
+     * @param[in] type: The texture type.
+     * @return Image::ColorSpace::SRGB or Image::ColorSpace::LINEAR.
+     */
     static Image::ColorSpace textureTypeToColorSpace(TextureType type)
     {
         switch (type) {
         case TextureType::BASE_COLOR:
-        case TextureType::EMISSIVE:
-            return Image::ColorSpace::SRGB;
+        case TextureType::EMISSIVE: return Image::ColorSpace::SRGB;
         case TextureType::METALLIC_ROUGHNESS:
         case TextureType::NORMAL:
         case TextureType::OCCLUSION:
-        default:
-            return Image::ColorSpace::LINEAR;
+        default: return Image::ColorSpace::LINEAR;
         }
     }
 };
