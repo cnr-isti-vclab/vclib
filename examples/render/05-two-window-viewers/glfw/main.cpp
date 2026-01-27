8a139bae30db706809a350cd746f4106d308dd7b
Luigi Malomo <malomo@users.noreply.github.com>
2026-01-27 18:48:40 +0100

    [render] add GLFW example with two viewer windows

---
+++ examples/render/05-two-window-viewers/glfw/main.cpp
@@ -0,0 +1,65 @@
+/*****************************************************************************
+ * VCLib                                                                     *
+ * Visual Computing Library                                                  *
+ *                                                                           *
+ * Copyright(C) 2021-2026                                                    *
+ * Visual Computing Lab                                                      *
+ * ISTI - Italian National Research Council                                  *
+ *                                                                           *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This program is free software; you can redistribute it and/or modify      *
+ * it under the terms of the Mozilla Public License Version 2.0 as published *
+ * by the Mozilla Foundation; either version 2 of the License, or            *
+ * (at your option) any later version.                                       *
+ *                                                                           *
+ * This program is distributed in the hope that it will be useful,           *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              *
+ * Mozilla Public License Version 2.0                                        *
+ * (https://www.mozilla.org/en-US/MPL/2.0/) for more details.                *
+ ****************************************************************************/
+
+#include "get_drawable_mesh.h"
+
+#include <vclib/glfw/viewer_window.h>
+
+int main(int argc, char** argv)
+{
+    vcl::glfw::ViewerWindow viewer1("Viewer GLFW 1");
+    vcl::glfw::ViewerWindow viewer2("Viewer GLFW 2");
+
+    // load and set up a drawable mesh
+    vcl::DrawableMesh<vcl::TriMesh> drawable = getDrawableMesh<vcl::TriMesh>();
+
+    using enum vcl::MeshRenderInfo::Buffers;
+    drawable.color() = vcl::Color::Yellow;
+    drawable.updateBuffers({MESH_UNIFORMS});
+
+    auto mrs = drawable.renderSettings();
+    mrs.setSurface(vcl::MeshRenderInfo::Surface::COLOR_MESH);
+    mrs.setSurface(vcl::MeshRenderInfo::Surface::SHADING_FLAT);
+    drawable.setRenderSettings(mrs);
+
+    // add the drawable mesh to the scene
+    // the viewer will own **a copy** of the drawable mesh
+    viewer1.pushDrawableObject(drawable);
+
+    viewer1.fitScene();
+
+    viewer1.show();
+
+    mrs.setSurface(vcl::MeshRenderInfo::Surface::SHADING_SMOOTH);
+    mrs.setSurface(vcl::MeshRenderInfo::Surface::COLOR_VERTEX);
+    drawable.setRenderSettings(mrs);
+
+    // add the drawable mesh to the scene
+    // the viewer will own **a copy** of the drawable mesh
+    viewer2.pushDrawableObject(drawable);
+
+    viewer2.fitScene();
+
+    viewer2.show();
+
+    return vcl::glfw::GLFWApplication::instance().exec();
+}
