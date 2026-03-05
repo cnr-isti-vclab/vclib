a97e313ad2f34929a8fe851c9b762a0b7d0b4cb2
alemuntoni <alemuntoni@users.noreply.github.com>
2026-03-05 09:05:40 +0100

    [render] first draft of Editor class

---
+++ vclib/render/include/vclib/render/editors/editor.h
@@ -0,0 +1,66 @@
+/*****************************************************************************
+ * VCLib                                                                     *
+ * Visual Computing Library                                                  *
+ *                                                                           *
+ * Copyright(C) 2021-2026                                                    *
+ * Visual Computing Lab                                                      *
+ * ISTI - Italian National Research Council                                  *
+ *                                                                           *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This program is free software; you can redistribute it and/or modify      *
+ * it under the terms of the Mozilla Public License Version 2.0 as published *
+ * by the Mozilla Foundation; either version 2 of the License, or            *
+ * (at your option) any later version.                                       *
+ *                                                                           *
+ * This program is distributed in the hope that it will be useful,           *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              *
+ * Mozilla Public License Version 2.0                                        *
+ * (https://www.mozilla.org/en-US/MPL/2.0/) for more details.                *
+ ****************************************************************************/
+
+#ifndef VCL_RENDER_EDITORS_EDITOR_H
+#define VCL_RENDER_EDITORS_EDITOR_H
+
+#include <vclib/render/drawable/drawable_object_vector.h>
+#include <vclib/render/settings/editor_settings.h>
+
+#include <vclib/base.h>
+
+namespace vcl {
+
+/**
+ * @brief The Editor abstract class is a base class for all the editors of the
+ * VCLib Render module.
+ *
+ * An editor is an object contained in an AbstractViewerDrawer that can "edit"
+ * the behavior of the viewer itself. For example, it can be a gizmo that allows
+ * to edit the position of a drawable object, or it can be a widget that allows
+ * draw additional elements on top of the viewer (e.g. a grid, or a bounding
+ * box).
+ */
+class Editor
+{
+    std::shared_ptr<DrawableObjectVector> mDrawList;
+
+    bool mIsActive = false;
+
+public:
+    Editor() = default;
+
+    bool isActive() const { return mIsActive; }
+
+    virtual void setActive(bool active) { mIsActive = active; }
+
+    void setDrawableObjectVector(const std::shared_ptr<DrawableObjectVector>& v)
+    {
+        mDrawList = v;
+    }
+
+    virtual void draw(const EditorSettings& settings) const = 0;
+};
+
+} // namespace vcl
+
+#endif // VCL_RENDER_EDITORS_EDITOR_H
---
+++ vclib/render/include/vclib/render/settings/editor_settings.h
@@ -0,0 +1,50 @@
+/*****************************************************************************
+ * VCLib                                                                     *
+ * Visual Computing Library                                                  *
+ *                                                                           *
+ * Copyright(C) 2021-2026                                                    *
+ * Visual Computing Lab                                                      *
+ * ISTI - Italian National Research Council                                  *
+ *                                                                           *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This program is free software; you can redistribute it and/or modify      *
+ * it under the terms of the Mozilla Public License Version 2.0 as published *
+ * by the Mozilla Foundation; either version 2 of the License, or            *
+ * (at your option) any later version.                                       *
+ *                                                                           *
+ * This program is distributed in the hope that it will be useful,           *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              *
+ * Mozilla Public License Version 2.0                                        *
+ * (https://www.mozilla.org/en-US/MPL/2.0/) for more details.                *
+ ****************************************************************************/
+
+#ifndef VCL_RENDER_SETTINGS_EDITOR_SETTINGS_H
+#define VCL_RENDER_SETTINGS_EDITOR_SETTINGS_H
+
+#include <vclib/base.h>
+
+namespace vcl {
+
+struct EditorSettings {
+
+    enum class EditMode {
+        NONE,
+        CURRENT_OBJECT,
+        VISIBLE_OBJECTS,
+        ALL_OBJECTS,
+    };
+
+    /**< @brief The edit mode of the editor. */
+    EditMode editMode = EditMode::CURRENT_OBJECT;
+
+    uint currentObjectId = UINT_NULL;
+
+    /**< @brief The view ID on which the editor can draw. */
+    uint viewId = 0;
+};
+
+} // namespace vcl
+
+#endif // VCL_RENDER_SETTINGS_EDITOR_SETTINGS_H
