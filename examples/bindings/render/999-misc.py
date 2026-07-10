8cffef7c2f4fe322c29cdd7e8c7831ad549b065b
alemuntoni <alemuntoni@users.noreply.github.com>
2026-07-10 14:31:34 +0200

    [examples] first bindings/render example

---
+++ examples/bindings/render/999-misc.py
@@ -0,0 +1,39 @@
+# VCLib - Visual Computing Library
+# Copyright (C) 2021-2026 Visual Computing Lab, ISTI - CNR.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file, You can
+# obtain one at https://mozilla.org/MPL/2.0/.
+
+# This example assumes that vclib is installed and available in the PYTHONPATH.
+
+import vclib as vcl
+import os
+import numpy as np
+
+import sys
+from PyQt6.QtWidgets import QApplication
+
+current_file_path = os.path.abspath(__file__)
+current_file_path = os.path.dirname(current_file_path)
+
+VCLIB_EXAMPLE_MESHES_PATH = current_file_path + "/../../../assets/example_meshes"
+VCLIB_PYTHON_RESULTS_PATH = current_file_path + "/../../../assets/results/python"
+
+if __name__ == "__main__":
+    print("=== VCLib Example 999: Miscellaneous ===\n")
+
+    app = QApplication(sys.argv)
+
+    m = vcl.load_tri_mesh('/home/alessandro/devel/3DMeshes/3DMeshes/abstract_sculpture.obj')
+
+    viewer = vcl.MeshViewer()
+
+    dm = vcl.make_drawable(m)
+
+    viewer.add_mesh(dm)
+
+    viewer.show()
+
+    sys.exit(app.exec())
+
