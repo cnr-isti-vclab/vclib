3635986d2257f38a0e8170412aef33243cc54374
alemuntoni <alemuntoni@users.noreply.github.com>
2026-07-27 18:05:22 +0200

    [3rdparty] glfw minimum version set to 3.4

--- 3rdparty/glfw.cmake
+++ 3rdparty/glfw.cmake
@@ -5,7 +5,7 @@
 # v. 2.0. If a copy of the MPL was not distributed with this file, You can
 # obtain one at https://mozilla.org/MPL/2.0/.
 
-find_package(glfw3 3 QUIET)
+find_package(glfw3 3.4 QUIET)
 
 if(VCLIB_ALLOW_SYSTEM_GLFW AND glfw3_FOUND)
     message(STATUS "- GLFW - using system-provided library")
