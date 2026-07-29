8441950b14f4589979cbd60896ef7ae92024c8ae
alemuntoni <alemuntoni@users.noreply.github.com>
2026-07-29 14:16:54 +0200

    [examples] 05-mesh-viewer-headless example

---
+++ examples/render/05-mesh-viewer-headless/main.cpp
@@ -0,0 +1,96 @@
+// VCLib - Visual Computing Library
+// Copyright (C) 2021-2026 Visual Computing Lab, ISTI - CNR.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License,
+// v. 2.0. If a copy of the MPL was not distributed with this file, You can
+// obtain one at https://mozilla.org/MPL/2.0/.
+
+#include "get_drawable_mesh.h"
+
+#include <vclib/render/headless_mesh_viewer.h>
+
+#include <vclib/space/core.h>
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    vcl::HeadlessMeshViewer mv("VCLib Headless Mesh Viewer");
+
+    // load and set up a drawable mesh
+    auto mesh = getDrawableMesh<vcl::TriMesh>();
+
+    auto mesh_scaled = mesh;
+
+    using enum vcl::MeshRenderInfo::Buffers;
+
+    // to test the per-vertex and per-face color rendering, we set both of them
+    mesh.enablePerVertexColor();
+    mesh.enablePerFaceColor();
+
+    for (auto& f : mesh.faces()) {
+        f.vertex(0)->color() = vcl::Color::Red;
+        f.vertex(1)->color() = vcl::Color::Green;
+        f.vertex(2)->color() = vcl::Color::Blue;
+        if (f.index() % 3 == 0)
+            f.color() = vcl::Color::Red;
+        else if (f.index() % 3 == 1)
+            f.color() = vcl::Color::Green;
+        else
+            f.color() = vcl::Color::Blue;
+    }
+    mesh.updateBuffers({VERT_COLORS, TRI_COLORS, WIREFRAME});
+
+    mv.pushDrawableObject(std::move(mesh));
+
+    // load and set up a drawable mesh
+    mesh_scaled.name() = "bimba_scaled";
+
+    // update the mesh to be displayed in the scene
+    const auto bb = vcl::boundingBox(mesh_scaled);
+
+    vcl::Matrix44d rot         = vcl::Matrix44d::Identity();
+    vcl::Matrix44d scale       = vcl::Matrix44d::Identity();
+    vcl::Matrix44d translation = vcl::Matrix44d::Identity();
+
+    vcl::setTransformMatrixRotation(
+        rot, vcl::Point3d(0, 1, 0), vcl::toRad(90.0));
+
+    vcl::setTransformMatrixScale(scale, 0.5);
+    vcl::setTransformMatrixTranslation(
+        translation, vcl::Point3d(bb.size().x(), 0, 0));
+
+    mesh_scaled.transformMatrix() = translation * scale * rot;
+
+    mesh_scaled.updateBuffers({MESH_ADDITIONAL_DATA});
+    mv.pushDrawableObject(std::move(mesh_scaled));
+
+    // center the camera on the scene
+    mv.fitScene();
+
+    // Rotate camera 25 degrees to the right along the y axis
+    vcl::Cameraf cam = mv.camera();
+
+    vcl::Point3f   dir    = cam.eye() - cam.center();
+    vcl::Matrix44f camRot = vcl::Matrix44f::Identity();
+    vcl::setTransformMatrixRotation(
+        camRot, vcl::Point3f(0.f, 1.f, 0.f), vcl::toRad(25.f));
+    vcl::Point3f newDir =
+        (camRot * vcl::Point4f(dir.x(), dir.y(), dir.z(), 0.f)).head<3>();
+
+    cam.eye() = cam.center() + newDir;
+
+    // Apply camera settings
+    mv.setCamera(cam);
+
+    std::string screenshotFilename = "screenshot.png";
+    std::cout << "Requesting screenshot: " << screenshotFilename << std::endl;
+
+    // Request screenshot and run rendering loop (auto concludes)
+    mv.screenshot(screenshotFilename);
+
+    std::cout << "Rendering loop finished. Check " << screenshotFilename
+              << " for the output." << std::endl;
+
+    return 0;
+}
