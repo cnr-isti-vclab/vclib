946a9376846dcce632d083502fc648b180154beb
alemuntoni <alemuntoni@users.noreply.github.com>
2026-08-07 17:36:02 +0200

    [examples] uniforming core, render/core and render/external examples cmake files

--- examples/standalone/core.cmake
+++ examples/standalone/core.cmake
@@ -16,7 +16,37 @@ file(
 foreach(dir IN LISTS CORE_EXAMPLE_DIRS)
     if(IS_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/../core/${dir}")
         if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/../core/${dir}/main.cpp")
-            add_standalone_example(core "${dir}")
+            # Reset required targets for this iteration
+            set(EXAMPLE_REQUIRED_TARGETS "")
+            if(
+                EXISTS
+                    "${CMAKE_CURRENT_SOURCE_DIR}/../core/${dir}/requirements.cmake"
+            )
+                include(
+                    "${CMAKE_CURRENT_SOURCE_DIR}/../core/${dir}/requirements.cmake"
+                )
+            endif()
+
+            # Verify if all the required targets (e.g., 3rd party libraries) exist
+            set(REQUIREMENTS_MET TRUE)
+            set(UNMET_REQ_PRINT "")
+            foreach(req IN LISTS EXAMPLE_REQUIRED_TARGETS)
+                if(NOT TARGET ${req} AND NOT TARGET vclib::${req})
+                    set(REQUIREMENTS_MET FALSE)
+                    string(REPLACE "vclib-3rd-" "" req_print ${req})
+                    list(APPEND UNMET_REQ_PRINT ${req_print})
+                endif()
+            endforeach()
+
+            if(REQUIREMENTS_MET)
+                add_standalone_example(core "${dir}")
+            else()
+                message(
+                    STATUS
+                    "Skipping ${dir} due to missing req: "
+                    "${UNMET_REQ_PRINT}"
+                )
+            endif()
         endif()
     endif()
 endforeach()
