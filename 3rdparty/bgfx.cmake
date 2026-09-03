04dae0bd9ceb292cca9ee8bb6ec90a013b3a94b6
alemuntoni <alemuntoni@users.noreply.github.com>
2026-09-03 10:07:41 +0200

    [3rdparty] upgrade bgfx

--- 3rdparty/bgfx.cmake
+++ 3rdparty/bgfx.cmake
@@ -5,7 +5,7 @@
 # v. 2.0. If a copy of the MPL was not distributed with this file, You can
 # obtain one at https://mozilla.org/MPL/2.0/.
 
-set(BGFX_VERSION 1.157.9447-569)
+set(BGFX_VERSION 1.157.9470-570)
 
 find_package(bgfx QUIET)
 
