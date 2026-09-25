d3deeb633515144778e82563f12a7c49fe95e25a
alemuntoni <alemuntoni@users.noreply.github.com>
2026-09-25 18:29:39 +0200

    [render] add undo/redo actions for rename and delete drawable objects

---
+++ vclib/render/include/vclib/qt/undo_redo_actions.h
@@ -0,0 +1,14 @@
+// VCLib - Visual Computing Library
+// Copyright (C) 2021-2026 Visual Computing Lab, ISTI - CNR.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License,
+// v. 2.0. If a copy of the MPL was not distributed with this file, You can
+// obtain one at https://mozilla.org/MPL/2.0/.
+
+#ifndef VCL_QT_UNDO_REDO_ACTIONS_H
+#define VCL_QT_UNDO_REDO_ACTIONS_H
+
+#include "undo_redo_actions/delete_drawable_object_action.h"
+#include "undo_redo_actions/rename_drawable_object_action.h"
+
+#endif // VCL_QT_UNDO_REDO_ACTIONS_H
